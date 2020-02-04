da0eb11235464fbedd4988325f2369346f5476e5
Brandon Baker <bbaker@edx.org>
2020-02-04 15:26:38 -0500

    Add packages back

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,34 @@
 import 'babel-polyfill';
 
+import {
+  initialize,
+  APP_INIT_ERROR,
+  APP_READY,
+  subscribe,
+} from '@edx/frontend-platform';
+import { ErrorPage, AppProvider } from '@edx/frontend-platform/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+subscribe(APP_READY, () => {
+  ReactDOM.render(
+    (
+      <AppProvider>
+        <App />
+      </AppProvider>
+    ),
+    document.getElementById('root'),
+  );
+});
+
+subscribe(APP_INIT_ERROR, (error) => {
+  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+});
+
+initialize({
+  messages: [],
+  requireAuthenticatedUser: true,
+  hydrateAuthenticatedUser: true,
+});
