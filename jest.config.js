a058107d3038758db3d87b6bdd441a0f4d750929
Brandon Baker <bbaker@edx.org>
2020-02-04 14:28:12 -0500

    Fix babel transform

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/fileTransformer.js',
   },
   testPathIgnorePatterns: ['node_modules', '.cache'],
-  transformIgnorePatterns: ['node_modules/(?!(gatsby|@edx/paragon|@edx/frontend-learner-portal-base))'],
+  transformIgnorePatterns: ['node_modules/(?!(gatsby|@edx/paragon|@edx/frontend-learner-portal-base|@edx/frontend-platform))'],
   globals: {
     __PATH_PREFIX__: '',
   },
