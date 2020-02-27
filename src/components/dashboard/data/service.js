2e1fdf31b549202b2bfa7475684ea13acc9ddf07
chavesj <jchaves@edx.org>
2020-02-27 10:51:53 -0500

    changing endpoint

--- src/components/dashboard/data/service.js
+++ src/components/dashboard/data/service.js
@@ -1,7 +1,7 @@
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 
 const fetchEntepriseCustomerConfig = (slug) => {
-  const url = `${process.env.LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/dashboard_list/?enterprise_slug=${slug}`;
+  const url = `${process.env.LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/?slug=${slug}`;
   return getAuthenticatedHttpClient().get(url);
 };
 
