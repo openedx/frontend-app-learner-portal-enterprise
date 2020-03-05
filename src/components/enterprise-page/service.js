1d8a4fffbdd5e284f9aa911d48f912ec18b2de78
Adam Stankiewicz <agstanki@gmail.com>
2020-03-05 13:45:24 -0500

    moving hooks and service files into data folder

--- src/components/enterprise-page/service.js
+++
@@ -1,9 +0,0 @@
-import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
-
-const fetchEntepriseCustomerConfig = (slug) => {
-  const url = `${process.env.LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/?slug=${slug}`;
-  return getAuthenticatedHttpClient().get(url);
-};
-
-// eslint-disable-next-line import/prefer-default-export
-export { fetchEntepriseCustomerConfig };
