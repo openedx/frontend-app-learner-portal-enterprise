92ce92fd9919698a1421d1af9032871183f5d07e
binodpant <bpant@edx.org>
2020-05-29 11:35:56 -0400

    lint fix

--- src/components/enterprise-page/data/tests/customerConfig.test.js
+++ src/components/enterprise-page/data/tests/customerConfig.test.js
@@ -1,17 +1,17 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks';
 
 import useEnterpriseCustomerConfig from '../hooks';
 
 describe('branding configuraton use cases', () => {
-    xtest('null branding_configuration uses default values and does not fail', () => {
-        const enterpriseConfig = renderHook(() => useEnterpriseCustomerConfig());
-        expect(enterpriseConfig).toBeDefined();
-    })
+  xtest('null branding_configuration uses default values and does not fail', () => {
+    const enterpriseConfig = renderHook(() => useEnterpriseCustomerConfig());
+    expect(enterpriseConfig).toBeDefined();
+  });
 
-    xtest('null values for fields in branding_config uses defaults and does not fail', () => {
-    })
+  xtest('null values for fields in branding_config uses defaults and does not fail', () => {
+  });
 
-    xtest('valid branding_config results in correct values for logo and other branding settings', () => {
+  xtest('valid branding_config results in correct values for logo and other branding settings', () => {
 
-    })
-})
+  });
+});
