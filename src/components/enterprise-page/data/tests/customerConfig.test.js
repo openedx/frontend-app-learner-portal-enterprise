2a7711a1a7a9ab9dc79259b47a75373c3fe54a6e
binodpant <bpant@edx.org>
2020-05-29 15:05:16 -0400

    upgrade react test renderer and a test (disabled)

--- src/components/enterprise-page/data/tests/customerConfig.test.js
+++ src/components/enterprise-page/data/tests/customerConfig.test.js
@@ -1,11 +1,40 @@
 import { renderHook } from '@testing-library/react-hooks';
 
-import useEnterpriseCustomerConfig from '../hooks';
+import { useEnterpriseCustomerConfig } from '../hooks';
+import { fetchEntepriseCustomerConfig } from '../service';
 
-describe('branding configuraton use cases', () => {
+jest.mock('../service');
+
+// for now let's mock router dom to mock useParams, but perhaps we can
+// pass a default valued arg into the hook to avoid this?
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({ enterpriseSlug: 'test-enterprise' }),
+}));
+
+const responseWithNullBrandingConfig = {
+  data: {
+    results: [
+      {
+        branding_configuration: null,
+        name: 'Test Enterprise',
+        enable_learner_portal: true,
+        replace_sensitive_sso_username: false,
+        site: { domain: 'example.com', name: 'example.com' },
+        slug: 'test-enterprise',
+        uuid: 'aa672add-2554-4a5e-9576-618ba63c5325',
+      },
+    ],
+  },
+};
+
+describe('customer config with various states of branding_configuration', () => {
   xtest('null branding_configuration uses default values and does not fail', () => {
-    const enterpriseConfig = renderHook(() => useEnterpriseCustomerConfig());
-    expect(enterpriseConfig).toBeDefined();
+    fetchEntepriseCustomerConfig.mockResolvedValue(responseWithNullBrandingConfig);
+
+    const { result } = renderHook(() => useEnterpriseCustomerConfig());
+
+    expect(result.current).toBeDefined();
   });
 
   xtest('null values for fields in branding_config uses defaults and does not fail', () => {
