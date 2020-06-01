9d2b9b1fccdb2faaabd2b9913e2c47f4d51735a1
binodpant <bpant@edx.org>
2020-06-01 09:34:06 -0400

    lint

--- src/components/enterprise-page/data/tests/customerConfig.test.js
+++ src/components/enterprise-page/data/tests/customerConfig.test.js
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 
 import { useEnterpriseCustomerConfig } from '../hooks';
 import { fetchEntepriseCustomerConfig } from '../service';
