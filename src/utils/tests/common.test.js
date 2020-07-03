8d7e4246e236a98b35e6db0acc4b2cc32d8cdff7
Adam Stankiewicz <agstanki@gmail.com>
2020-07-03 08:13:35 -0400

    fix course sidebar null effort bug

---
+++ src/utils/tests/common.test.js
@@ -0,0 +1,75 @@
+import {
+  isDefinedAndNotNull,
+  createArrayFromValue,
+  isDefinedAndNull,
+} from '../common';
+
+function assertTestCaseEquals(testCase, expectedValue) {
+  const result = createArrayFromValue(testCase);
+  expect(result).toEqual(expectedValue);
+}
+
+describe('createArrayFromValue', () => {
+  it('handles array value', () => {
+    const testCase = [null, null];
+    assertTestCaseEquals(testCase, testCase);
+  });
+
+  it('handles other values', () => {
+    let testCase = 2;
+    assertTestCaseEquals(testCase, [testCase]);
+
+    testCase = { test: 'key' };
+    assertTestCaseEquals(testCase, [testCase]);
+
+    testCase = 2;
+    assertTestCaseEquals(testCase, [testCase]);
+
+    testCase = undefined;
+    assertTestCaseEquals(testCase, [testCase]);
+  });
+});
+
+describe('isDefinedAndNotNull', () => {
+  it('returns false when passed a null', () => {
+    const result = isDefinedAndNotNull(null);
+    expect(result).toBeFalsy();
+  });
+
+  it('returns false when passed an array of nulls', () => {
+    const result = isDefinedAndNotNull([null, null]);
+    expect(result).toBeFalsy();
+  });
+
+  it('returns false when passed an array with 1 null', () => {
+    const result = isDefinedAndNotNull(['test', null]);
+    expect(result).toBeFalsy();
+  });
+
+  it('returns true when passed an array with no nulls', () => {
+    const result = isDefinedAndNotNull(['test', 1]);
+    expect(result).toBeTruthy();
+  });
+});
+
+describe('isDefinedAndNull', () => {
+  it('returns true when passed a null', () => {
+    const result = isDefinedAndNull(null);
+    expect(result).toBeTruthy();
+  });
+
+  it('returns true when passed an array of nulls', () => {
+    const result = isDefinedAndNull([null, null]);
+    expect(result).toBeTruthy();
+  });
+
+  it('returns false when passed an array with 1 null', () => {
+    const result = isDefinedAndNull(['test', null]);
+    expect(result).toBeFalsy();
+  });
+
+  it('returns false when passed an array with no nulls', () => {
+    const result = isDefinedAndNull(['test', 1]);
+    expect(result).toBeFalsy();
+  });
+});
