7a8b3528110ca3a5321623ac7905c7485d3cb8aa
Brandon Baker <bbaker6225@users.noreply.github.com>
2020-07-23 11:18:23 -0400

    Redirect unauthenticated users to proxy-login (#68)
    ENT-3142

--- src/components/course/CoursePage.jsx
+++ src/components/course/CoursePage.jsx
@@ -3,18 +3,21 @@ import React from 'react';
 import { EnterprisePage } from '../enterprise-page';
 import { EnterpriseBanner } from '../enterprise-banner';
 import { Layout } from '../layout';
+import { LoginRedirect } from '../login-redirect';
 import { UserSubsidy } from '../enterprise-user-subsidy';
 import Course from './Course';
 
 export default function CoursePage() {
   return (
-    <EnterprisePage>
-      <Layout>
-        <EnterpriseBanner />
-        <UserSubsidy>
-          <Course />
-        </UserSubsidy>
-      </Layout>
-    </EnterprisePage>
+    <LoginRedirect>
+      <EnterprisePage>
+        <Layout>
+          <EnterpriseBanner />
+          <UserSubsidy>
+            <Course />
+          </UserSubsidy>
+        </Layout>
+      </EnterprisePage>
+    </LoginRedirect>
   );
 }
--- src/components/dashboard/DashboardPage.jsx
+++ src/components/dashboard/DashboardPage.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 
 import { EnterprisePage } from '../enterprise-page';
 import { Layout } from '../layout';
+import { LoginRedirect } from '../login-redirect';
 import { UserSubsidy } from '../enterprise-user-subsidy';
 import { EnterpriseBanner } from '../enterprise-banner';
 import Dashboard from './Dashboard';
 
 export default function DashboardPage() {
   return (
-    <EnterprisePage>
-      <Layout>
-        <EnterpriseBanner />
-        <UserSubsidy>
-          <Dashboard />
-        </UserSubsidy>
-      </Layout>
-    </EnterprisePage>
+    <LoginRedirect>
+      <EnterprisePage>
+        <Layout>
+          <EnterpriseBanner />
+          <UserSubsidy>
+            <Dashboard />
+          </UserSubsidy>
+        </Layout>
+      </EnterprisePage>
+    </LoginRedirect>
   );
 }
