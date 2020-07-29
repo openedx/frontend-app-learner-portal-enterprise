280612bd73d60d96bfb8682fb9b3bd83d94aa729
Alex Dusenbery <adusenbery@edx.org>
2020-07-29 16:23:49 -0400

    During license-activation, refresh the user's JWT so that we can see any available roles that might have been granted since the JWT was originally acquired.

--- src/components/license-activation/data/service.js
+++ src/components/license-activation/data/service.js
@@ -1,7 +1,25 @@
+import Cookies from 'universal-cookie';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import qs from 'query-string';
 
-export function activateLicense(activationKey) {
+export async function activateLicense(activationKey) {
+  // If the user has not refreshed their JWT since they created their account,
+  // we should refresh it so that they'll have appropriate roles (if available),
+  // and thus, have any appropriate permissions when making downstream requests.
+  const loginRefreshUrl = `${process.env.LMS_BASE_URL}/login_refresh`;
+
+  try {
+    await getAuthenticatedHttpClient().post(loginRefreshUrl);
+  } catch (error) {
+    const isUserUnauthenticated = error.response?.status === 401;
+    if (isUserUnauthenticated) {
+      // Clean up the cookie if it exists to eliminate any situation
+      // where the cookie is not expired but the jwt is expired.
+      const cookies = new Cookies();
+      cookies.remove(process.env.ACCESS_TOKEN_COOKIE_NAME);
+    }
+  }
+
   const queryParams = { activation_key: activationKey };
   const url = `${process.env.LICENSE_MANAGER_URL}/api/v1/license-activation/?${qs.stringify(queryParams)}`;
 
