db0d13a49f90c9adec501240bcb9d04bc3d0eb8b
Lael Birch <lbirch@edx.org>
2020-08-18 16:33:30 -0400

    Update wording and avoid showing zero while data is loading Update wording and add icon
    Remove the zero showing when loading

--- src/components/enterprise-banner/OffersAlert.jsx
+++ src/components/enterprise-banner/OffersAlert.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Alert } from '@edx/paragon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 import { fetchOffers } from '../dashboard/sidebar/offers';
 
-export const getOffersText = (number) => `You have ${number} offers available.`;
+export const getOffersText = (number) => `You have ${number} course redemption voucher${number > 1 ? 's' : ''} left to use.`;
 
 export class BaseOffersAlert extends React.Component {
   componentDidMount() {
@@ -18,17 +20,20 @@ export class BaseOffersAlert extends React.Component {
 
   render() {
     const { offersCount } = this.props;
-    return (
-      <>{offersCount && (
+    if (offersCount) {
+      return (
         <Alert
           className="pl-5"
           variant="info"
         >
-          <div className="container">{getOffersText(offersCount)}</div>
+          <div className="container">
+            <FontAwesomeIcon className="mr-1" icon={faInfoCircle} />
+            {getOffersText(offersCount)}
+          </div>
         </Alert>
-      )}
-      </>
-    );
+      );
+    }
+    return null;
   }
 }
 
