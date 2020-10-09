387d1daec5d0860ceb77d4c308f1f3d3f20e0a1c
Lael Birch <lbirch@edx.org>
2020-10-09 15:32:38 -0400

    Move common wrappers to a Page component

--- src/components/course/CoursePage.jsx
+++ src/components/course/CoursePage.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 
-import { EnterprisePage } from '../enterprise-page';
-import { EnterpriseBanner } from '../enterprise-banner';
-import { Layout } from '../layout';
-import { LoginRedirect } from '../login-redirect';
-import { UserSubsidy } from '../enterprise-user-subsidy';
 import Course from './Course';
+import AuthenticatedPage from '../app/AuthenticatedPage';
 
 export default function CoursePage() {
   return (
-    <LoginRedirect>
-      <EnterprisePage>
-        <Layout>
-          <EnterpriseBanner />
-          <UserSubsidy>
-            <Course />
-          </UserSubsidy>
-        </Layout>
-      </EnterprisePage>
-    </LoginRedirect>
+    <AuthenticatedPage>
+      <Course />
+    </AuthenticatedPage>
   );
 }
