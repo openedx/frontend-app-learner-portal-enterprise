aa33904f538f5fd771f5de6d192d72792443a0af
Kellie Selinka <kellie.selinka@gmail.com>
2020-11-04 15:12:28 -0500

    add front end caching layer to requests that are made multiple times (#118)
    * add caching layer
    
    * change to use options object
    
    * Use caching for a couple other endpoints
    
    * upgrade frontend-platform and frontend-build
    
    Co-authored-by: Kellie Selinka <>
    Co-authored-by: Adam Stankiewicz <agstanki@gmail.com>

--- src/components/enterprise-page/data/service.js
+++ src/components/enterprise-page/data/service.js
@@ -3,16 +3,13 @@ import qs from 'query-string';
 
 export function fetchEnterpriseCustomerConfig(slug) {
   const url = `${process.env.LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/?slug=${slug}`;
-  return getAuthenticatedHttpClient().get(url);
+  return getAuthenticatedHttpClient({ useCache: true }).get(url);
 }
 
 export function fetchEnterpriseCustomerSubscriptionPlan(enterpriseUuid) {
   const queryParams = {
     enterprise_customer_uuid: enterpriseUuid,
-    // TODO: provide `is_active=true` query param to only return subscription plans that
-    // are active to avoid needing to iterate through?
-    // is_active: true,
   };
   const url = `${process.env.LICENSE_MANAGER_URL}/api/v1/learner-subscriptions/?${qs.stringify(queryParams)}`;
-  return getAuthenticatedHttpClient().get(url);
+  return getAuthenticatedHttpClient({ useCache: true }).get(url);
 }
--- src/components/enterprise-user-subsidy/offers/data/service.js
+++ src/components/enterprise-user-subsidy/offers/data/service.js
@@ -2,7 +2,7 @@ import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 
 const fetchOffers = (query) => {
   const offersUrl = `${process.env.ECOMMERCE_BASE_URL}/api/v2/enterprise/offer_assignment_summary/?${query}`;
-  return getAuthenticatedHttpClient().get(offersUrl);
+  return getAuthenticatedHttpClient({ useCache: true }).get(offersUrl);
 };
 
 export { fetchOffers };
