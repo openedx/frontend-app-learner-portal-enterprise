0c2ca6b23ac501acb0961b77ed0af853e6305ac1
Adam Stankiewicz <agstanki@gmail.com>
2020-11-05 07:58:30 -0500

    delay spinner visibility with css only

--- src/components/loading-spinner/LoadingSpinner.jsx
+++ src/components/loading-spinner/LoadingSpinner.jsx
@@ -1,34 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const LOADING_TIMEOUT_DELAY = 100;
+import './styles/LoadingSpinner.scss';
 
-const LoadingSpinner = ({
-  screenReaderText,
-}) => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(
-    () => {
-      setTimeout(() => {
-        setIsVisible(true);
-      }, LOADING_TIMEOUT_DELAY);
-    },
-    [],
-  );
-
-  if (!isVisible) {
-    return null;
-  }
-
-  return (
-    <div className="d-flex justify-content-center align-items-center">
-      <div className="spinner-border text-primary" role="status">
-        <span className="sr-only">{screenReaderText}</span>
-      </div>
+const LoadingSpinner = ({ screenReaderText }) => (
+  <div className="loading-spinner d-flex justify-content-center align-items-center">
+    <div className="spinner-border text-primary" role="status">
+      <span className="sr-only">{screenReaderText}</span>
     </div>
-  );
-};
+  </div>
+);
 
 LoadingSpinner.propTypes = {
   screenReaderText: PropTypes.string.isRequired,
