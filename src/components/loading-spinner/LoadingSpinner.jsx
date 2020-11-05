3542e44a30784665d54871b81b7c009ef74ba694
Adam Stankiewicz <agstanki@gmail.com>
2020-11-05 07:04:11 -0500

    disable api cache during dev, enable in prod

--- src/components/loading-spinner/LoadingSpinner.jsx
+++ src/components/loading-spinner/LoadingSpinner.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const LoadingSpinner = (props) => {
-  const { screenReaderText } = props;
+const LOADING_TIMEOUT_DELAY = 100;
+
+const LoadingSpinner = ({
+  screenReaderText,
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(
+    () => {
+      setTimeout(() => {
+        setIsVisible(true);
+      }, LOADING_TIMEOUT_DELAY);
+    },
+    [],
+  );
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <div className="d-flex justify-content-center align-items-center">
       <div className="spinner-border text-primary" role="status">
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 export const FEATURE_ENROLL_WITH_CODES = 'ENROLL_WITH_CODES';
 
 // Subscription expiration constants
