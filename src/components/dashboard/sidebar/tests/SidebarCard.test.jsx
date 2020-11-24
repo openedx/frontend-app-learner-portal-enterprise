96610c9e6eb50ec709489f89c0de499bdf70761c
binodpant <bpant@edx.org>
2020-11-23 19:51:26 -0500

    feat: sidebarCard var name fix

--- src/components/dashboard/sidebar/tests/SidebarCard.test.jsx
+++ src/components/dashboard/sidebar/tests/SidebarCard.test.jsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { screen } from '@testing-library/react';
 
 import { renderWithRouter } from '../../../../utils/tests';
-import SideboardCard from '../SidebarCard';
+import SidebarCard from '../SidebarCard';
 
 describe('<SidebarCard />', () => {
   const childrenText = 'Some text goes here';
@@ -12,12 +12,12 @@ describe('<SidebarCard />', () => {
     buttonLink: 'http://bears.party',
   };
   it('renders the card children', () => {
-    renderWithRouter(<SideboardCard {...defaultProps} />);
+    renderWithRouter(<SidebarCard {...defaultProps} />);
     expect(screen.getByText(childrenText)).toBeTruthy();
   });
   it('renders a card title', () => {
     const title = 'Here be dragons';
-    renderWithRouter(<SideboardCard {...defaultProps} title={title} />);
+    renderWithRouter(<SidebarCard {...defaultProps} title={title} />);
     expect(screen.getByText(title)).toBeTruthy();
   });
 });
