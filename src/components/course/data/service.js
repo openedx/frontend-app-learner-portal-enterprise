5789dc202a38e80701c7805e91a381990292eeac
binodpant <bpant@edx.org>
2021-01-23 10:49:29 -0500

    feat: make it todo

--- src/components/course/data/service.js
+++ src/components/course/data/service.js
@@ -155,7 +155,7 @@ export default class CourseService {
 
   /**
    * Returns an offer whose catalog uuid matches one of the provided catalogs.
-   * Note: This method currently cannot help discover if the offer applies specifically
+   * TODO: This method currently cannot help discover if the offer applies specifically
    * to a course, just that there is an offer that matches one in the list of catalogs.
    * @param {Object} args Arguments
    * @param {Array<Offer>} args.offers All offers for this user for an enterprise
