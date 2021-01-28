bcba9e9746b3b3ad545e792b683f29f0c841469d
binodpant <bpant@edx.org>
2021-01-28 11:36:39 -0500

    feat: constants and hooks, tests pending

---
+++ src/components/course/enrollment/constants.js
@@ -0,0 +1,11 @@
+const enrollButtonTypesLocal = {
+  ENROLL_DISABLED: 'enroll_disabled',
+  TO_COURSEWARE_PAGE: 'to_courseware_page',
+  VIEW_ON_DASHBOARD: 'view_on_dashboard',
+  TO_DATASHARING_CONSENT: 'to_datasharing_consent',
+  TO_ECOM_BASKET: 'to_ecom_basket',
+  TO_VOUCHER_REDEEM: 'to_voucher_redeem',
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const enrollButtonTypes = Object.freeze(enrollButtonTypesLocal);
---
+++ src/components/course/enrollment/hooks.js
@@ -0,0 +1,109 @@
+import { useMemo, useContext } from 'react';
+
+import { AppContext } from '@edx/frontend-platform/react';
+
+import { UserSubsidyContext } from '../../enterprise-user-subsidy/UserSubsidy';
+import { useCourseEnrollmentUrl } from '../data/hooks';
+import { CourseContext } from '../CourseContextProvider';
+import {
+  findHighestLevelSeatSku, findOfferForCourse, hasCourseStarted, findUserEnrollmentForCourse,
+} from '../data/utils';
+
+/**
+ * Enrollment data needed for enroll logic
+ * @typedef {Object} EnrollmentData
+ * @property {boolean} isUserEnrolled
+ * @property {boolean} isCourseStarted
+ * @property {boolean} isEnrollable
+ * @property {object} userEnrollment
+ */
+
+/**
+ *
+ * Extracts info needed for enroll button logic from CourseContext, for a single course.
+ * Which course?
+ *   `activeCourseRun`, `userEnrollments` are looked up in the CourseContext's `state`
+ *
+ * @returns {EnrollmentData} enrolldata data for use in enrollment logic
+ */
+export function useEnrollData() {
+  const { state: courseData } = useContext(CourseContext);
+  const { activeCourseRun, userEnrollments } = courseData;
+  const { key, start, isEnrollable } = activeCourseRun;
+
+  const isCourseStarted = useMemo(
+    () => hasCourseStarted(start),
+    [start],
+  );
+
+  const userEnrollment = useMemo(
+    () => findUserEnrollmentForCourse({ userEnrollments, key }),
+    [userEnrollments, key],
+  );
+  return {
+    isEnrollable,
+    isUserEnrolled: !!userEnrollment,
+    isCourseStarted,
+    userEnrollment,
+  };
+}
+
+/**
+ * Extract subsidy information from CourseContext and UserSubsidyContext.
+ * Before calling this, ensure required data is in CourseContext, UserSubsidyContext and AppContext
+ *
+ * CourseContext: must have `state` with `activeCourseRun`, `userSubsidy` and `catalog`
+ * AppContext: must have `enterpriseConfig` populated
+ * UserSubsidyContext: must have `subscriptionLicense`, `offers` populated
+ *
+ * @param { object } location router location. Only here because of useEnrollmentUrl hook.
+ *   (TODO Refactor this once useEnrollmentUrl is refactored!)
+ *
+ * @returns {object} with fields:
+ * {
+ *    subscriptionLicense,
+ *    userSubsidy,
+ *    enrollmentUrl,
+ *    offersCount,
+*     courseHasOffer,
+ * }
+ */
+export function useSubsidyData({ location }) {
+  const { state: courseData } = useContext(CourseContext);
+  const { enterpriseConfig } = useContext(AppContext);
+  const { subscriptionLicense, offers: { offers, offersCount } } = useContext(UserSubsidyContext);
+
+  const {
+    activeCourseRun,
+    userSubsidy,
+    catalog: { catalogList },
+  } = courseData;
+  const {
+    key,
+    seats,
+  } = activeCourseRun;
+
+  const sku = useMemo(
+    () => findHighestLevelSeatSku(seats),
+    [seats],
+  );
+
+  const enrollmentUrl = useCourseEnrollmentUrl({
+    catalogList,
+    enterpriseConfig,
+    key,
+    location,
+    offers,
+    sku,
+    subscriptionLicense,
+    userSubsidy,
+  });
+
+  return {
+    subscriptionLicense,
+    userSubsidy,
+    enrollmentUrl,
+    offersCount,
+    courseHasOffer: !!findOfferForCourse(offers, catalogList),
+  };
+}
