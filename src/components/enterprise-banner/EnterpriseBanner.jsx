8cd286be6a333a9580f12966b00da3f1db6e710b
Adam Stankiewicz <agstanki@gmail.com>
2021-01-29 21:16:00 -0500

    change enterprise banner h1 to be styled as an h2

--- src/components/enterprise-banner/EnterpriseBanner.jsx
+++ src/components/enterprise-banner/EnterpriseBanner.jsx
@@ -8,7 +8,7 @@ export default function EnterpriseBanner() {
   return (
     <div className="enterprise-banner bg-brand-secondary">
       <div className="container-fluid">
-        <h1 className="mb-0 py-3 pl-3 border-brand-tertiary text-brand-secondary">
+        <h1 className="h2 mb-0 py-3 pl-3 border-brand-tertiary text-brand-secondary">
           {enterpriseConfig.name}
         </h1>
       </div>
