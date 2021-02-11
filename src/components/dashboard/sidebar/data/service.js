bcc775673a004aa4f9492f4d84c82b833881eff0
Adam Stankiewicz <agstanki@gmail.com>
2021-02-11 11:07:25 -0500

    remove client caching from activity call

--- src/components/dashboard/sidebar/data/service.js
+++ src/components/dashboard/sidebar/data/service.js
@@ -5,8 +5,5 @@ export const fetchRecentCommunityActivityFeed = () => {
   const config = getConfig();
   // TODO: limit this to most recent 5 activities
   const url = `${config.LMS_BASE_URL}/enterprise/api/v1/enterprise-activity/`;
-  const httpClient = getAuthenticatedHttpClient({
-    useCache: config.USE_API_CACHE,
-  });
-  return httpClient.get(url);
+  return getAuthenticatedHttpClient().get(url);
 };
