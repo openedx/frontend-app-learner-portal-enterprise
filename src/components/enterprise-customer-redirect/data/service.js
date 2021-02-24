50c215704bf7c380a94471674737b6c2e9cdda7c
Adam Stankiewicz <astankiewicz@edx.org>
2021-02-24 18:13:21 -0500

    fetch enterprise customer by selected uuid (#217)

--- src/components/enterprise-customer-redirect/data/service.js
+++ src/components/enterprise-customer-redirect/data/service.js
@@ -1,8 +1,9 @@
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { getConfig } from '@edx/frontend-platform/config';
 
-export function fetchEnterpriseCustomersForUser() {
+export function fetchEnterpriseCustomerByUUID(enterpriseUUID) {
   const config = getConfig();
-  const url = `${config.LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/`;
+
+  const url = `${config.LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/?uuid=${enterpriseUUID}`;
   return getAuthenticatedHttpClient().get(url);
 }
