1de6439ef3195174a0cee79e750ff04dd2adbfb8
Lael Birch <lbirch@edx.org>
2021-03-02 12:04:35 -0500

    Move useDefaultSearchFilters back to learner portal (#228)
    This function is only relevant to this repo
    Uses the user's license instead of the subscription plan to determine if user has a license

---
+++ src/components/search/data/hooks.js
@@ -0,0 +1,52 @@
+import { useContext, useMemo, useEffect } from 'react';
+import {
+  SearchContext, getCatalogString, SHOW_ALL_NAME, setRefinementAction,
+} from '@edx/frontend-enterprise';
+import { features } from '../../../config';
+import { LICENSE_STATUS } from '../../enterprise-user-subsidy/data/constants';
+
+export const useDefaultSearchFilters = ({
+  enterpriseConfig, subscriptionPlan, subscriptionLicense, offerCatalogs = [],
+}) => {
+  // default to showing all catalogs
+  const { refinementsFromQueryParams, dispatch } = useContext(SearchContext);
+
+  useEffect(() => {
+    // if the user has no subscriptions or offers, we default to showing all catalogs
+    if (!(subscriptionLicense?.status === LICENSE_STATUS.ACTIVATED) && offerCatalogs.length < 1) {
+      dispatch(setRefinementAction(SHOW_ALL_NAME, 1));
+    }
+  }, [subscriptionLicense?.status, offerCatalogs.length]);
+
+  const filters = useMemo(
+    () => {
+      if (refinementsFromQueryParams[SHOW_ALL_NAME]) {
+        // show all enterprise catalogs
+        return `enterprise_customer_uuids:${enterpriseConfig.uuid}`;
+      }
+      // if there's a subscriptionPlan, filter results by the subscription catalog
+      // and any catalogs for which the user has vouchers
+      if (subscriptionLicense?.status === LICENSE_STATUS.ACTIVATED) {
+        if (features.ENROLL_WITH_CODES && offerCatalogs.length > 0) {
+          const catalogs = [subscriptionPlan.enterpriseCatalogUuid, ...offerCatalogs];
+          return getCatalogString(catalogs);
+        }
+        return `enterprise_catalog_uuids:${subscriptionPlan.enterpriseCatalogUuid}`;
+      }
+      if (features.ENROLL_WITH_CODES && offerCatalogs.length > 0) {
+        // shows catalogs for which a user has 100% vouchers
+        return getCatalogString(offerCatalogs);
+      }
+      return `enterprise_customer_uuids:${enterpriseConfig.uuid}`;
+    },
+    [
+      enterpriseConfig,
+      subscriptionPlan,
+      offerCatalogs,
+      refinementsFromQueryParams[SHOW_ALL_NAME],
+      subscriptionLicense?.status,
+    ],
+  );
+
+  return { filters };
+};
---
+++ src/components/search/data/tests/hooks.test.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { renderHook } from '@testing-library/react-hooks';
+import '@testing-library/jest-dom/extend-expect';
+import {
+  getCatalogString, SearchContext, SearchData, SHOW_ALL_NAME,
+} from '@edx/frontend-enterprise';
+import {
+  useDefaultSearchFilters,
+} from '../hooks';
+import { LICENSE_STATUS } from '../../../enterprise-user-subsidy/data/constants';
+
+const TEST_ENTERPRISE_UUID = 'test-enterprise-uuid';
+const TEST_SUBSCRIPTION_CATALOG_UUID = 'test-subscription-catalog-uuid';
+
+jest.mock('react-router-dom', () => ({
+  useLocation: () => ({
+    search: '?q=test%20query&subjects=Computer%20Science,Communication&availability=Upcoming&ignore=true',
+  }),
+  useHistory: () => ({ push: jest.fn }),
+}));
+jest.mock('../../../../config', () => ({
+  features: { ENROLL_WITH_CODES: true },
+}));
+
+const SearchWrapper = (value) => ({ children }) => (
+  <SearchContext.Provider value={value}>{children}</SearchContext.Provider>);
+
+describe('useDefaultSearchFilters hook', () => {
+  const enterpriseConfig = { uuid: TEST_ENTERPRISE_UUID };
+  const subscriptionPlan = { enterpriseCatalogUuid: TEST_SUBSCRIPTION_CATALOG_UUID };
+  const validSubscriptionLicense = { status: LICENSE_STATUS.ACTIVATED };
+  const invalidSubscriptionLicense = { status: LICENSE_STATUS.ASSIGNED };
+  const refinementsFromQueryParamsShowAll = { refinementsFromQueryParams: { [SHOW_ALL_NAME]: 1 } };
+  describe('no catalogs', () => {
+    test('no subscription: returns enterprise customer uuid as filter', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig,
+      }), { wrapper: SearchData });
+      const { filters } = result.current;
+      expect(filters).toBeDefined();
+      expect(filters).toEqual(`enterprise_customer_uuids:${TEST_ENTERPRISE_UUID}`);
+    });
+
+    test('with valid subscription: returns subscription catalog uuid as filter', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig, subscriptionPlan, subscriptionLicense: validSubscriptionLicense,
+      }), { wrapper: SearchData });
+      const { filters } = result.current;
+      expect(filters).toBeDefined();
+      expect(filters).toEqual(`enterprise_catalog_uuids:${TEST_SUBSCRIPTION_CATALOG_UUID}`);
+    });
+
+    test('with invalid subscription: returns enterprise customer uuid as a filter', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig,
+        subscriptionPlan,
+        subscriptionLicense: invalidSubscriptionLicense,
+      }), { wrapper: SearchData });
+      const { filters } = result.current;
+      expect(filters).toBeDefined();
+      expect(filters).toEqual(`enterprise_customer_uuids:${TEST_ENTERPRISE_UUID}`);
+    });
+    test('with valid subscription and showAllCatalogs: returns subscription and all enterprise catalogs', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig,
+        subscriptionPlan,
+        subscriptionLicense: validSubscriptionLicense,
+      }), { wrapper: SearchWrapper({ ...refinementsFromQueryParamsShowAll }) });
+
+      const { filters } = result.current;
+      expect(filters).toEqual(`enterprise_customer_uuids:${TEST_ENTERPRISE_UUID}`);
+    });
+  });
+  describe('with catalogs', () => {
+    const offerCatalogs = ['catalog1', 'catalog2'];
+    test('with valid subscription: returns subscription and offers', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig,
+        subscriptionPlan,
+        subscriptionLicense: validSubscriptionLicense,
+        offerCatalogs,
+      }), { wrapper: SearchData });
+      const { filters } = result.current;
+      expect(filters).toBeDefined();
+      expect(filters)
+        .toEqual(`enterprise_catalog_uuids:${TEST_SUBSCRIPTION_CATALOG_UUID} OR ${getCatalogString(offerCatalogs)}`);
+    });
+    test('with invalid subscription: returns offer catalogs', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig,
+        subscriptionPlan,
+        subscriptionLicense: invalidSubscriptionLicense,
+        offerCatalogs,
+      }), { wrapper: SearchData });
+      const { filters } = result.current;
+      expect(filters).toBeDefined();
+      expect(filters)
+        .toEqual(getCatalogString(offerCatalogs));
+    });
+    test('no subscription: returns only offers', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig,
+        subscriptionPlan: null,
+        offerCatalogs,
+      }), { wrapper: SearchData });
+      const { filters } = result.current;
+      expect(filters).toBeDefined();
+      expect(filters).toEqual(getCatalogString(offerCatalogs));
+    });
+    test('with showAllCatalogs: returns all enterprise catalgos', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig,
+        subscriptionPlan: null,
+        offerCatalogs,
+      }), { wrapper: SearchWrapper({ ...refinementsFromQueryParamsShowAll }) });
+      const { filters } = result.current;
+      expect(filters).toEqual(`enterprise_customer_uuids:${TEST_ENTERPRISE_UUID}`);
+    });
+    test('with valid subscription and show all: returns all enterprise catalogs', () => {
+      const { result } = renderHook(() => useDefaultSearchFilters({
+        enterpriseConfig,
+        subscriptionPlan,
+        subscriptionLicense: validSubscriptionLicense,
+        offerCatalogs,
+      }), { wrapper: SearchWrapper({ ...refinementsFromQueryParamsShowAll }) });
+      const { filters } = result.current;
+      expect(filters).toEqual(`enterprise_customer_uuids:${TEST_ENTERPRISE_UUID}`);
+    });
+  });
+});
