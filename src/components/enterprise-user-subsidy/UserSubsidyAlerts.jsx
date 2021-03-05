7a2546f8c20f935a56c0d09163073171e2af1710
Talia Rhodes <trhodes@edx.org>
2021-03-05 13:37:40 -0500

    Remove unnecessary banner from dashboard.

--- src/components/enterprise-user-subsidy/UserSubsidyAlerts.jsx
+++ src/components/enterprise-user-subsidy/UserSubsidyAlerts.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { AppContext } from '@edx/frontend-platform/react';
 import { UserSubsidyContext } from '.';
-import OffersAlert from './OffersAlert';
 import SubscriptionSubsidy from './SubscriptionSubsidy';
 
 const UserSubsidyAlerts = () => {
@@ -10,7 +9,6 @@ const UserSubsidyAlerts = () => {
 
   return (
     <>
-      {offers && <OffersAlert offers={offers} />}
       {/**
        * SubscriptionSubsidy takes care of redirecting the user to `/${enterpriseConfig.slug}`
        * if their organization has a subscription plan but they don't have appropriate access
--- src/components/search/SearchPage.jsx
+++ src/components/search/SearchPage.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { SearchData } from '@edx/frontend-enterprise';
 import Search from './Search';
 import AuthenticatedUserSubsidyPage from '../app/AuthenticatedUserSubsidyPage';
+import OffersAlert from '../enterprise-user-subsidy/OffersAlert';
 
 const SearchPage = () => (
   <AuthenticatedUserSubsidyPage>
+    <OffersAlert />
     <SearchData>
       <Search />
     </SearchData>
