f412b2b48880caa149c6b02908334acf1616dcf3
Sameen Fatima <55431213+sameenfatima78@users.noreply.github.com>
2021-06-16 13:17:51 +0500

    fix: added character limit for skills Popover (#272)

--- src/components/course/CourseSkills.jsx
+++ src/components/course/CourseSkills.jsx
@@ -7,7 +7,10 @@ import {
 
 import { AppContext } from '@edx/frontend-platform/react';
 import { CourseContext } from './CourseContextProvider';
-import { SKILLS_BUTTON_LABEL, SKILL_DESCRIPTION_PLACEHOLDER } from './data/constants';
+import {
+  SKILLS_BUTTON_LABEL, SKILL_DESCRIPTION_PLACEHOLDER, SKILL_DESCRIPTION_CUTOFF_LIMIT, ELLIPSIS_STR,
+} from './data/constants';
+import { shortenString } from './data/utils';
 
 export const MAX_VISIBLE_SKILLS = 5;
 
@@ -27,12 +30,15 @@ export default function CourseSkills() {
             key={skill.name}
             placement="top"
             overlay={(
-              <Popover id="popover-positioned-top" style={{ maxWidth: 380 }}>
+              <Popover id="popover-positioned-top" style={{ maxWidth: 460 }}>
                 <Popover.Title as="h5">{skill.name}</Popover.Title>
                 <Popover.Content
                   className={classNames({ 'text-muted': !skill.description, 'font-italic': !skill.description })}
                 >
-                  { skill.description ? skill.description : SKILL_DESCRIPTION_PLACEHOLDER }
+                  {
+                    skill.description ? shortenString(skill.description, SKILL_DESCRIPTION_CUTOFF_LIMIT, ELLIPSIS_STR)
+                      : SKILL_DESCRIPTION_PLACEHOLDER
+                  }
                 </Popover.Content>
               </Popover>
             )}
--- src/components/course/tests/testUtils.js
+++ src/components/course/tests/testUtils.js
@@ -16,7 +16,7 @@ Example:
   }
 ]
 */
-const generateRandomSkills = (skillCount) => {
+export const generateRandomSkills = (skillCount) => {
   const skillsData = Array(skillCount).fill(1).map((item, index) => ({
     name: `Test skill ${++index}`, // eslint-disable-line no-param-reassign
     description: `Description for Test Skill ${index}`,
@@ -24,4 +24,25 @@ const generateRandomSkills = (skillCount) => {
   return skillsData;
 };
 
-export default generateRandomSkills;
+export const generateRandomString = (MAX_LENGTH) => {
+  // form strings b/w 5-10 characters
+  const min = Math.ceil(5);
+  const max = Math.floor(10);
+  const result = [];
+  let stringLength = 0;
+  let arrStr = '';
+  while (stringLength < MAX_LENGTH) {
+    const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+    const arr = new Array(randomInt).fill('s');
+    arrStr = arr.join('');
+    result.push(arrStr);
+    stringLength = stringLength + randomInt + 1; // + 1 for space b/w strings
+  }
+  // there might be a chance that we get a string larger than MX_LENGTH based on the random integer value,
+  // to ensure the length is equal to MAX_LENGTH passed, we slice the string
+  let resStr = result.join(' ');
+  if (resStr.length > MAX_LENGTH) {
+    resStr = resStr.substr(0, MAX_LENGTH);
+  }
+  return resStr;
+};
