abbf5abcf15407060e3f658355126794210659e2
Adam Stankiewicz <agstanki@gmail.com>
2021-06-23 09:46:13 -0400

    fix: allow access to search and course pages for learners with no subsidy

--- src/components/dashboard/main-content/DashboardMainContent.jsx
+++ src/components/dashboard/main-content/DashboardMainContent.jsx
@@ -1,36 +1,29 @@
 import React, { useContext } from 'react';
-import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { AppContext } from '@edx/frontend-platform/react';
+import { Button } from '@edx/paragon';
 
 import { CourseEnrollments } from './course-enrollments';
 
-import { UserSubsidyContext } from '../../enterprise-user-subsidy';
-
 const DashboardMainContent = () => {
   const { enterpriseConfig: { name, slug } } = useContext(AppContext);
-  const { hasAccessToPortal } = useContext(UserSubsidyContext);
 
   return (
     <CourseEnrollments>
-      {/*
-          The children below will only be rendered if there are no course runs.
-       */}
+      {/* The children below will only be rendered if there are no course runs. */}
       <h2>Find a Course</h2>
       <p>
         You are not enrolled in any courses sponsored by {name}.
         To start taking a course, browse the catalog below.
       </p>
       <p>
-        <Link
+        <Button
+          as={Link}
           to={`/${slug}/search`}
-          className={classNames(
-            'btn', 'btn-primary', 'btn-brand-primary',
-            { disabled: !hasAccessToPortal },
-          )}
+          className="btn-brand-primary"
         >
           Find a course
-        </Link>
+        </Button>
       </p>
     </CourseEnrollments>
   );
