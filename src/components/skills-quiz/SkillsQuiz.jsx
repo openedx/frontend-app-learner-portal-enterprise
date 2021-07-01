5177a1f6f85fe4c1d5c348eea4e842d2e5eed35a
Saleem Latif <saleem.latif.ee@gmail.com>
2021-07-01 12:21:45 +0500

    Creation of the page with the title bar, summary and Continue button is complete.

---
+++ src/components/skills-quiz/SkillsQuiz.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+// import { AppContext } from '@edx/frontend-platform/react';
+
+import {
+  Container, Row,
+} from '@edx/paragon';
+
+import { MainContent } from '../layout';
+import SkillsQuizStepper from './SkillsQuizStepper';
+
+const SkillsQuiz = () => {
+  // const { enterpriseConfig } = useContext(AppContext);
+  // const PAGE_TITLE = `Skills Quiz - ${enterpriseConfig.name}`;
+  const PAGE_TITLE = 'Skills Quiz';
+
+  return (
+    <>
+      <Helmet title={PAGE_TITLE} />
+      <Container size="lg" className="py-5">
+        <Row>
+          <MainContent>
+            <SkillsQuizStepper />
+          </MainContent>
+        </Row>
+
+      </Container>
+    </>
+  );
+};
+
+export default SkillsQuiz;
