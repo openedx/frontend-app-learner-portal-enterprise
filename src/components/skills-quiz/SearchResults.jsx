d43f5749fb52b49d47aa3ff7b584a1ead2186d8e
Zaman Afzal <zamanafzal@gmail.com>
2021-08-10 14:42:26 +0500

    feat: Select Job card to show courses (#287)

--- src/components/skills-quiz/SearchResults.jsx
+++ src/components/skills-quiz/SearchResults.jsx
@@ -8,7 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 import SearchJobCard from './SearchJobCard';
-import { JOBS_ERROR_ALERT_MESSAGE } from './constants';
+import SelectJobCard from './SelectJobCard';
+import { JOBS_ERROR_ALERT_MESSAGE, STEP1, STEP2 } from './constants';
 import { isDefinedAndNotNull } from '../../utils/common';
 
 const renderError = () => (
@@ -26,23 +27,26 @@ const SearchResults = ({
   searchResults,
   isSearchStalled,
   error,
+  currentStep,
+  className,
 }) => {
   const nbHits = useNbHitsFromSearchResults(searchResults);
-
   return (
-    <Container size="lg" className="search-results my-5">
+    <Container size="lg" className={`my-5 ${className}`}>
       <>
         {isSearchStalled && (
           <>
             <Skeleton className="lead mb-4" width={160} />
             <div className="skeleton-job-card">
-              <SearchJobCard.Skeleton />
+              {currentStep === STEP2 ? (
+                <SelectJobCard.Skeleton />)
+                : (<SearchJobCard.Skeleton />)}
             </div>
           </>
         )}
         {!isSearchStalled && nbHits > 0 && (
           <>
-            <Hits hitComponent={SearchJobCard} />
+            <Hits hitComponent={currentStep === STEP2 ? SelectJobCard : SearchJobCard} />
           </>
         )}
         {!isSearchStalled && isDefinedAndNotNull(error) && (
@@ -64,12 +68,16 @@ SearchResults.propTypes = {
   }),
   isSearchStalled: PropTypes.bool,
   error: PropTypes.shape(),
+  currentStep: PropTypes.string,
+  className: PropTypes.string,
 };
 
 SearchResults.defaultProps = {
   searchResults: undefined,
   isSearchStalled: false,
   error: undefined,
+  currentStep: STEP1,
+  className: 'search-results',
 };
 
 export default connectStateResults(SearchResults);
---
+++ src/components/skills-quiz/SelectJobCard.jsx
@@ -0,0 +1,101 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
+// TODO: Uncomment this line when jobs are coming as hits from Algolia
+// import { camelCaseObject } from '@edx/frontend-platform/utils';
+import { Card, Form } from '@edx/paragon';
+import { SearchContext, setRefinementAction } from '@edx/frontend-enterprise-catalog-search';
+
+const SelectJobCard = ({ hit, isLoading }) => { // eslint-disable-line no-unused-vars
+  const { refinementsFromQueryParams, dispatch } = useContext(SearchContext);
+  const { selectedJob } = refinementsFromQueryParams;
+  const { skill_names: skills } = refinementsFromQueryParams;
+
+  const jobs = skills?.length > 0 ? skills.slice(0, 3) : null;
+  // This statement will be usable once we have jobs data available in Algolia
+  // Currently we are showing skills data in place of that, 'hit' will be passed as props to SearchJobCard
+  // const job = hit ? camelCaseObject(hit) : {};
+  const jobsCharactersCutOffLimit = 14;
+
+  return (
+    <>
+      <Form.Group>
+        <Form.RadioSet
+          name="selected-job"
+          onChange={(e) => dispatch(setRefinementAction('selectedJob', e.target.value))}
+          defaultValue={selectedJob}
+          isInline
+          className="row"
+        >
+
+          {jobs?.map(job => (
+            <div
+              key={job}
+              className="mb-3 col-md-3 offset-md-1"
+              role="group"
+              aria-label={job}
+            >
+              <Card className={`${selectedJob === job ? 'border border-dark' : null}`}>
+                <Card.Body>
+                  <Card.Title as="h5" className="card-title mb-1">
+                    {isLoading ? (
+                      <Skeleton count={1} data-testid="job-title-loading" />
+                    ) : (
+                      <>
+                        {job.length > jobsCharactersCutOffLimit
+                          ? `${job.substring(0, jobsCharactersCutOffLimit)}...` : job}
+                      </>
+                    )}
+                    <Form.Radio value={job} />
+                  </Card.Title>
+
+                  {isLoading ? (
+                    <Skeleton duration={0} data-testid="job-content-loading" />
+                  ) : (
+                    <>
+                      <div>
+                        <span>Median Salary:</span>
+                        <span className="text-muted">
+                          $110K
+                        </span>
+                      </div>
+                      <div>
+                        <span>Job Postings:</span>
+                        <span className="text-muted">
+                          6.5k
+                        </span>
+                      </div>
+                    </>
+                  )}
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </Form.RadioSet>
+      </Form.Group>
+    </>
+  );
+};
+
+const SkeletonSelectJobCard = (props) => (
+  <SelectJobCard {...props} isLoading />
+);
+
+SelectJobCard.propTypes = {
+  hit: PropTypes.shape({
+    key: PropTypes.string,
+    title: PropTypes.string,
+    medianSalary: PropTypes.string,
+    jobPostings: PropTypes.string,
+  }),
+  isLoading: PropTypes.bool,
+};
+
+SelectJobCard.defaultProps = {
+  hit: undefined,
+  isLoading: false,
+};
+
+SelectJobCard.Skeleton = SkeletonSelectJobCard;
+
+export default SelectJobCard;
