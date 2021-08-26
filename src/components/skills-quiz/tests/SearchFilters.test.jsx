cf5f238cfe0bad280126e0758e19c0caa7db7399
Muneeb Ur Rehman <muneeb.github@gmail.com>
2021-08-26 16:02:59 +0500

    update SkillsQuizStepper dropdown tests

--- src/components/skills-quiz/tests/SearchFilters.test.jsx
+++ src/components/skills-quiz/tests/SearchFilters.test.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { SKILLS_QUIZ_FACET_FILTERS, JOBS_QUIZ_FACET_FILTERS } from '../constants';
+import { JOBS_QUIZ_FACET_FILTERS, SKILLS_FACET, CURRENT_JOB_FACET } from '../constants';
 
 import { renderWithSearchContext } from './utils';
 
 import '../__mocks__/react-instantsearch-dom';
 import SkillsQuizStepper from '../SkillsQuizStepper';
 
+const facetsToTest = [JOBS_QUIZ_FACET_FILTERS, SKILLS_FACET, CURRENT_JOB_FACET];
 describe('<SkillsQuizStepper />', () => {
   test('renders skills and jobs dropdown with a label', () => {
     renderWithSearchContext(<SkillsQuizStepper />);
-    SKILLS_QUIZ_FACET_FILTERS.forEach((filter) => {
+    facetsToTest.forEach((filter) => {
       expect(screen.getByText(filter.title)).toBeInTheDocument();
     });
     expect(screen.getByText(JOBS_QUIZ_FACET_FILTERS.title)).toBeInTheDocument();
