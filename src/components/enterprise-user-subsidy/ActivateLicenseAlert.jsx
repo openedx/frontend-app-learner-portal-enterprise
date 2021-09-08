643bed617dce75f54a407d461b2cf724f7ee49eb
Adam Stankiewicz <agstanki@gmail.com>
2021-09-08 17:11:52 -0400

    fix: remove activation cta from banner

--- src/components/enterprise-user-subsidy/ActivateLicenseAlert.jsx
+++ src/components/enterprise-user-subsidy/ActivateLicenseAlert.jsx
@@ -10,33 +10,16 @@ import { UserSubsidyContext } from './UserSubsidy';
  * activated. Provides a CTA button that links to the license activation route.
  */
 const ActivateLicenseAlert = () => {
-  const { enterpriseSlug } = useParams();
   const { subscriptionLicense } = useContext(UserSubsidyContext);
   if (!subscriptionLicense || ['activated', 'revoked'].includes(subscriptionLicense.status)) {
     return null;
   }
-  const { activationKey } = subscriptionLicense;
   return (
     <Container size="lg" className="mt-3">
       <Alert variant="warning">
-        <div className="d-flex align-items-center justify-content-between">
-          <span>
-            Your subscription license is not activated. To enroll in courses without
-            payment, you must activate your license.
-          </span>
-          <Button
-            as={Link}
-            variant="primary"
-            size="sm"
-            to={`/${enterpriseSlug}/licenses/${activationKey}/activate`}
-            onClick={() => {
-              sendTrackEvent('edx.ui.enterprise.learner_portal.activate_license_alert.activate_cta.clicked');
-            }}
-            data-testid="activateCta"
-          >
-            Activate now
-          </Button>
-        </div>
+        Your subscription license is not activated. To enroll in courses without
+        payment, you must activate your license from the activation link sent to
+        your email.
       </Alert>
     </Container>
   );
