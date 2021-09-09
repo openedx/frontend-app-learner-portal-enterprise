063b03dd82a283e59ed0467afbfb084103a7059b
zamanafzal <zamanafzal@gmail.com>
2021-09-09 16:17:55 +0500

    feat: Adding Skills Quiz button on course search page

--- src/components/enterprise-banner/EnterpriseBanner.jsx
+++ src/components/enterprise-banner/EnterpriseBanner.jsx
@@ -1,17 +1,32 @@
 import React, { useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { AppContext } from '@edx/frontend-platform/react';
-import { Container } from '@edx/paragon';
+import { Button, Container } from '@edx/paragon';
 
 import './styles/EnterpriseBanner.scss';
 
 export default function EnterpriseBanner() {
+  const location = useLocation();
   const { enterpriseConfig } = useContext(AppContext);
+  const isSearchPage = `/${ enterpriseConfig.slug }/search` === location.pathname;
   return (
     <div className="enterprise-banner bg-brand-secondary">
       <Container size="lg">
-        <h1 className="h2 mb-0 py-3 pl-3 border-brand-tertiary text-brand-secondary">
-          {enterpriseConfig.name}
-        </h1>
+        <div className="row banner-content">
+          <h1 className="h2 mb-0 py-3 pl-3 border-brand-tertiary text-brand-secondary">
+            {enterpriseConfig.name}
+          </h1>
+          {isSearchPage
+          && (
+            <Button
+              as={Link}
+              to={`/${ enterpriseConfig.slug }/skills-quiz`}
+              variant="inverse-primary"
+              className="skills-quiz-btn"
+            > Skills Quiz
+            </Button>
+          )}
+        </div>
       </Container>
     </div>
   );
