9fe7feb0afe3794b15f61e989b59f681eed24a4e
Muneeb Ur Rehman <muneeb.github@gmail.com>
2021-09-27 15:50:53 +0500

    refactor: change name of parameter, and occurrence of median salary

--- src/components/utils/skills-quiz.js
+++ src/components/utils/skills-quiz.js
@@ -1,8 +1,8 @@
 import { DROPDOWN_OPTION_IMPROVE_CURRENT_ROLE } from '../skills-quiz/constants';
 
 // eslint-disable-next-line import/prefer-default-export
-export function checkValidGoalAndJobSelected(goal, jobs, goalIsImprove) {
-  return (goalIsImprove
+export function checkValidGoalAndJobSelected(goal, jobs, checkGoaIsImprove) {
+  return (checkGoaIsImprove
     ? goal === DROPDOWN_OPTION_IMPROVE_CURRENT_ROLE
     : goal !== DROPDOWN_OPTION_IMPROVE_CURRENT_ROLE)
     && (jobs?.length > 0);
