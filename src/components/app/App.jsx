bfef5d93f1618a4e283136663222c1370661c9ae
Thomas Tracy <contact@tjtracy.com>
2021-10-14 13:27:56 -0400

    feat: MICROBA-1521 notices redirect
    To support the notices plugin on platform, this adds a redirect to the course home page. If a user lands on that page and has not acknowledged a notice, the user will be redirected to notice instead of the course home. The user can back out of the notice, or acknowledge it. They will continue to see the notice every time they hit any course until it is acknowledged.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,6 +4,7 @@ import useHotjar from 'react-use-hotjar';
 import { AppProvider, AuthenticatedPageRoute, PageRoute } from '@edx/frontend-platform/react';
 
 import NotFoundPage from '../NotFoundPage';
+import NoticesProvider from '../notices-provider';
 import {
   EnterpriseCustomerRedirect,
   EnterprisePageRedirect,
@@ -26,17 +27,19 @@ export default function App() {
   }
   return (
     <AppProvider store={store}>
-      <Switch>
-        <AuthenticatedPageRoute exact path="/" component={EnterpriseCustomerRedirect} />
-        <AuthenticatedPageRoute exact path="/r/:redirectPath+" component={EnterprisePageRedirect} />
-        <PageRoute exact path="/:enterpriseSlug" component={DashboardPage} />
-        <PageRoute exact path="/:enterpriseSlug/search" component={SearchPage} />
-        <PageRoute exact path="/:enterpriseSlug/course/:courseKey" component={CoursePage} />
-        <PageRoute exact path="/:enterpriseSlug/program/:programUuid" component={ProgramPage} />
-        <PageRoute exact path="/:enterpriseSlug/licenses/:activationKey/activate" component={LicenseActivationPage} />
-        <PageRoute exact path="/:enterpriseSlug/skills-quiz" component={SkillsQuizPage} />
-        <PageRoute path="*" component={NotFoundPage} />
-      </Switch>
+      <NoticesProvider>
+        <Switch>
+          <AuthenticatedPageRoute exact path="/" component={EnterpriseCustomerRedirect} />
+          <AuthenticatedPageRoute exact path="/r/:redirectPath+" component={EnterprisePageRedirect} />
+          <PageRoute exact path="/:enterpriseSlug" component={DashboardPage} />
+          <PageRoute exact path="/:enterpriseSlug/search" component={SearchPage} />
+          <PageRoute exact path="/:enterpriseSlug/course/:courseKey" component={CoursePage} />
+          <PageRoute exact path="/:enterpriseSlug/program/:programUuid" component={ProgramPage} />
+          <PageRoute exact path="/:enterpriseSlug/licenses/:activationKey/activate" component={LicenseActivationPage} />
+          <PageRoute exact path="/:enterpriseSlug/skills-quiz" component={SkillsQuizPage} />
+          <PageRoute path="*" component={NotFoundPage} />
+        </Switch>
+      </NoticesProvider>
     </AppProvider>
   );
 }
