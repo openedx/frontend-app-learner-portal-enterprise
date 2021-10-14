e5883a0ad7e90df557d6d295ee367a16c32e0052
Adam Stankiewicz <agstanki@gmail.com>
2021-10-14 10:03:15 -0400

    fix: add activation link to alert (#340)
    * fix: add activation link to alert
    
    * fix: linting error
    
    * build: upgrade Paragon to bring in test fix
    
    * fix: add segment event on activate btn click

--- src/components/enterprise-user-subsidy/tests/ActivateLicenseAlert.test.jsx
+++ src/components/enterprise-user-subsidy/tests/ActivateLicenseAlert.test.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import '@testing-library/jest-dom/extend-expect';
+import { AppContext } from '@edx/frontend-platform/react';
 import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import ActivateLicenseAlert from '../ActivateLicenseAlert';
 
@@ -10,16 +11,20 @@ import { renderWithRouter } from '../../../utils/tests';
 
 const TEST_ENTERPRISE_SLUG = 'test-slug';
 
+// eslint-disable-next-line react/prop-types
+const ActivateLicenseAlertWrapper = ({ subscriptionLicense }) => (
+  <Route path="/:enterpriseSlug">
+    <AppContext.Provider value={{ enterpriseConfig: { slug: TEST_ENTERPRISE_SLUG } }}>
+      <UserSubsidyContext.Provider value={{ subscriptionLicense }}>
+        <ActivateLicenseAlert />
+      </UserSubsidyContext.Provider>
+    </AppContext.Provider>
+  </Route>
+);
+
 describe('<ActivateLicenseAlert />', () => {
   test('does not render alert when no license exists', () => {
-    const Component = (
-      <Route path="/:enterpriseSlug">
-        <UserSubsidyContext.Provider value={{ subscriptionLicense: null }}>
-          <ActivateLicenseAlert />
-        </UserSubsidyContext.Provider>
-      </Route>
-    );
-    renderWithRouter(Component, {
+    renderWithRouter(<ActivateLicenseAlertWrapper subscriptionLicense={null} />, {
       route: `/${TEST_ENTERPRISE_SLUG}`,
     });
     expect(screen.queryByRole('alert')).not.toBeInTheDocument();
@@ -29,31 +34,19 @@ describe('<ActivateLicenseAlert />', () => {
     ['activated', 'revoked'],
   )('does not render alert when license status is %s', (status) => {
     const subscriptionLicense = { status };
-    const Component = (
-      <Route path="/:enterpriseSlug">
-        <UserSubsidyContext.Provider value={{ subscriptionLicense }}>
-          <ActivateLicenseAlert />
-        </UserSubsidyContext.Provider>
-      </Route>
-    );
-    renderWithRouter(Component, {
+    renderWithRouter(<ActivateLicenseAlertWrapper subscriptionLicense={subscriptionLicense} />, {
       route: `/${TEST_ENTERPRISE_SLUG}`,
     });
     expect(screen.queryByRole('alert')).not.toBeInTheDocument();
   });
 
   test('renders alert when license status is assigned', () => {
-    const subscriptionLicense = { status: 'assigned' };
-    const Component = (
-      <Route path="/:enterpriseSlug">
-        <UserSubsidyContext.Provider value={{ subscriptionLicense }}>
-          <ActivateLicenseAlert />
-        </UserSubsidyContext.Provider>
-      </Route>
-    );
-    renderWithRouter(Component, {
+    const subscriptionLicense = { status: 'assigned', activationKey: 'test-uuid' };
+    renderWithRouter(<ActivateLicenseAlertWrapper subscriptionLicense={subscriptionLicense} />, {
       route: `/${TEST_ENTERPRISE_SLUG}`,
     });
     expect(screen.getByRole('alert')).toBeTruthy();
+    const activationLink = `/${TEST_ENTERPRISE_SLUG}/licenses/${subscriptionLicense.activationKey}/activate`;
+    expect(screen.getByRole('link')).toHaveAttribute('href', activationLink);
   });
 });
