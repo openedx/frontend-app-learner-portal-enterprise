f5fa38dedfaf1aabd7c262fee4c6821fd620b210
HammadAhmadWaqas <hammadahmadwaqas1@gmail.com>
2021-10-18 13:32:38 +0500

    feat: add Program sidebar, program overview and what will you learn for program.

--- src/components/program/ProgramMainContent.jsx
+++ src/components/program/ProgramMainContent.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ProgramInstructors from './ProgramInstructors';
 import ProgramCourses from './ProgramCourses';
+import { PreviewExpand } from '../preview-expand';
+import { ProgramContext } from './ProgramContextProvider';
+import BulletList from './BulletList';
 
-const ProgramMainContent = () => (
-  <div>
-    Program Main Content Placeholder
-    <ProgramInstructors />
-    <ProgramCourses />
-  </div>
-);
+const ProgramMainContent = () => {
+  const { program } = useContext(ProgramContext);
+  const { expectedLearningItems } = program;
+  return (
+    <div>
+      {expectedLearningItems && expectedLearningItems.length > 0 && (
+        <PreviewExpand
+          className="mb-5"
+          cta={{
+            labelToExpand: 'Expand what you\'ll learn',
+            labelToMinimize: 'Collapse what you\'ll learn',
+            id: 'what-youll-learn',
+          }}
+          heading={<h3>What you&apos;ll learn</h3>}
+        >
+          <div><BulletList items={expectedLearningItems} /></div>
+        </PreviewExpand>
+      )}
+      {program.overview && (
+        <PreviewExpand
+          className="mb-5"
+          cta={{
+            labelToExpand: 'More about this program',
+            labelToMinimize: 'Collapse about this program',
+            id: 'about-this-course',
+          }}
+          heading={<h3>About this program</h3>}
+        >
+          {/* eslint-disable-next-line react/no-danger */}
+          <div dangerouslySetInnerHTML={{ __html: program.overview }} />
+        </PreviewExpand>
+      )}
+      <ProgramInstructors />
+      <ProgramCourses />
+    </div>
+  );
+};
 
 export default ProgramMainContent;
