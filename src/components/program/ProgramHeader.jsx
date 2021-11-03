046dc6d4399deca7b97e988cf3a31b4d9ac1e6fb
Zaman Afzal <zamanafzal@gmail.com>
2021-11-03 13:33:30 +0500

    feat: add banner at the top of B2B Program Detail page (#364)
    * feat: add banner at the top of B2B Program Detail page

--- src/components/program/ProgramHeader.jsx
+++ src/components/program/ProgramHeader.jsx
@@ -1,17 +1,59 @@
 import React, { useContext } from 'react';
-
-import { Container } from '@edx/paragon';
+import { Parallax } from 'react-parallax';
+import { breakpoints } from '@edx/paragon';
+import { getConfig } from '@edx/frontend-platform/config';
 import { ProgramContext } from './ProgramContextProvider';
 
 const ProgramHeader = () => {
-  const { program: { title } } = useContext(ProgramContext);
+  const config = getConfig();
+  const { program: { subjects, marketingHook } } = useContext(ProgramContext);
+
+  let isMobileWindow = true;
+
+  // Use the first subject as the primary subject
+  const primarySubject = subjects?.length > 0 ? subjects[0] : '';
+  const subjectSlug = primarySubject?.slug ? primarySubject?.slug.toLowerCase() : '';
+
+  const handleResize = () => {
+    const isMobileWindowRefresh = window.matchMedia(`(max-width: ${breakpoints.small.maxWidth}px)`).matches;
+    if (isMobileWindow !== isMobileWindowRefresh) {
+      isMobileWindow = !isMobileWindow;
+    }
+  };
+
+  const getHeaderBackgroundImage = () => {
+    let subjectFolder = 'data-science';
+    if (subjectSlug === 'computer-science' || subjectSlug === 'business-management') {
+      subjectFolder = subjectSlug;
+    }
+    handleResize();
+    const imageSize = isMobileWindow ? 'sm' : 'lg';
+    return `${config.MARKETING_SITE_BASE_URL}/images/experiments/ProgramDetails/${subjectFolder}/hook-background-${imageSize}.jpg`;
+  };
+
+  const backgroundImage = getHeaderBackgroundImage();
 
   return (
-    <div className="program-header pt-5">
-      <Container size="lg">
-        <h1>{title}</h1>
-      </Container>
-    </div>
+    <>
+      {subjectSlug
+        ? (
+          <Parallax
+            blur={0}
+            bgImage={backgroundImage}
+            bgImageAlt=""
+            strength={600}
+          >
+            <header className="program-header">
+              <div className="container mw-lg program-header-container">
+                {/* this div is added to add breadcrumbs at the top of banner. */}
+                <div className="header-breadcrumbs ml-2" />
+                <h1 className="display-3">{marketingHook}</h1>
+              </div>
+            </header>
+          </Parallax>
+        )
+        : ''}
+    </>
   );
 };
 
