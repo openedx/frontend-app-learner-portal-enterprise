8d604d20f25219182a2caf0828198c2592ea00a5
Hammad Ahmad Waqas <hammadahmadwaqas1@gmail.com>
2021-11-17 16:39:31 +0500

    feat: update facet to 'program_type' for `Program` search (#392)

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 import qs from 'query-string';
-import { FEATURE_ENROLL_WITH_CODES, FEATURE_ENABLE_PROGRAMS } from './constants';
+import { FEATURE_ENROLL_WITH_CODES, FEATURE_ENABLE_PROGRAMS, FEATURE_PROGRAM_TYPE_FACET } from './constants';
 
 const hasFeatureFlagEnabled = (featureFlag) => {
   const { features } = qs.parse(window.location.search);
@@ -9,6 +9,7 @@ const hasFeatureFlagEnabled = (featureFlag) => {
 const features = {
   ENROLL_WITH_CODES: process.env.FEATURE_ENROLL_WITH_CODES || hasFeatureFlagEnabled(FEATURE_ENROLL_WITH_CODES),
   ENABLE_PROGRAMS: (process.env.FEATURE_ENABLE_PROGRAMS === 'true') || hasFeatureFlagEnabled(FEATURE_ENABLE_PROGRAMS),
+  PROGRAM_TYPE_FACET: (process.env.FEATURE_PROGRAM_TYPE_FACET === 'true') || hasFeatureFlagEnabled(FEATURE_PROGRAM_TYPE_FACET),
 };
 
 export { features };
