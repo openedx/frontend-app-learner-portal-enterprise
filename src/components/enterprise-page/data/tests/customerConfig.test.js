9f61ccae9699fb43f393743949acbe13c8eac3d6
Adam Stankiewicz <agstanki@gmail.com>
2021-11-19 16:45:51 -0500

    fix: include identity provider in enterpriseConfig metadata (#397)
    * fix: include identity provider in enterpriseConfig metadata
    
    * fix: enable auto-applied licenses feature in dev

--- src/components/enterprise-page/data/tests/customerConfig.test.js
+++ src/components/enterprise-page/data/tests/customerConfig.test.js
@@ -30,6 +30,11 @@ const responseWithNullBrandingConfig = {
         site: { domain: 'example.com', name: 'example.com' },
         slug: TEST_ENTERPRISE_SLUG,
         uuid: 'aa672add-2554-4a5e-9576-618ba63c5325',
+        identity_provider: 'saml-test',
+        hide_course_original_price: false,
+        hide_labor_market_data: false,
+        contact_email: 'edx@example.com',
+        enable_integrated_customer_learner_portal_search: true,
       },
     ],
   },
@@ -89,6 +94,25 @@ describe('useEnterpriseCustomerConfig', () => {
 
     const customerConfig = result.current[0];
     expect(customerConfig).toBeNull();
+  });
+  test('customer with learner portal enabled has correct result data', async () => {
+    fetchEnterpriseCustomerConfigForSlug.mockResolvedValue(responseWithBrandingConfig);
+
+    const { result, waitForNextUpdate } = renderHook(() => useEnterpriseCustomerConfig(TEST_ENTERPRISE_SLUG));
+    // since there is an async fetch in the hook
+    await waitForNextUpdate();
+
+    const expectedCustomerConfig = responseWithBrandingConfig.data.results[0];
+    const actualCustomerConfig = result.current[0];
+
+    expect(result.error).not.toBeDefined();
+    expect(actualCustomerConfig.name).toEqual(expectedCustomerConfig.name);
+    expect(actualCustomerConfig.uuid).toEqual(expectedCustomerConfig.uuid);
+    expect(actualCustomerConfig.slug).toEqual(expectedCustomerConfig.slug);
+    expect(actualCustomerConfig.contactEmail).toEqual(expectedCustomerConfig.contact_email);
+    expect(actualCustomerConfig.hideCourseOriginalPrice).toEqual(expectedCustomerConfig.hide_course_original_price);
+    expect(actualCustomerConfig.hideLaborMarketData).toEqual(expectedCustomerConfig.hide_labor_market_data);
+    expect(actualCustomerConfig.identityProvider).toEqual(expectedCustomerConfig.identity_provider);
   });
   describe('customer config with various states of branding_configuration', () => {
     test('null branding_configuration uses default values and does not fail', async () => {
