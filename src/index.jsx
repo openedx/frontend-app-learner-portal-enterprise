71add7f8846ee10739ddeb0776e12ec78d1b10c8
Eugene <42930224+Eugene-Kuzmenko@users.noreply.github.com>
2022-04-09 04:25:32 +0300

    [DAD-345] Add Sentry to all frontends and to the template (#4)
    * Introduce logging service
    
    * Set logging service properly. Fix logging
    
    * Move SentryLoggingService
    
    * Fix sentry

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,7 @@ import { mergeConfig } from '@edx/frontend-platform/config';
 import { App } from './components/app';
 
 import './index.scss';
+import { SentryLoggingService } from './utils/errorHandling';
 
 subscribe(APP_READY, () => {
   ReactDOM.render(<App />, document.getElementById('root'));
@@ -41,9 +42,13 @@ initialize({
         ENABLE_SKILLS_QUIZ: process.env.ENABLE_SKILLS_QUIZ || false,
         ENABLE_NOTICES: process.env.ENABLE_NOTICES || null,
         LEARNER_SUPPORT_URL: process.env.LEARNER_SUPPORT_URL || null,
+        SENTRY_DSN: process.env.SENTRY_DSN || null,
+        SENTRY_ENVIRONMENT: process.env.SENTRY_ENVIRONMENT || '',
+        SENTRY_PROJECT_ENV_PREFIX: 'dojo-frontend-app-learner-portal'
       });
     },
   },
+  loggingService: SentryLoggingService,
   messages: [],
   // We don't require authenticated users so that we can perform our own auth redirect to a proxy login that depends on
   // the route, rather than the LMS like frontend-platform does.
