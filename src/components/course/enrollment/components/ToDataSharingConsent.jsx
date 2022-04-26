d74a5542bd1f663c6b650c42d2a59c9d5701c8ae
Alex Dusenbery <adusenbery@edx.org>
2022-04-26 13:15:47 -0400

    feat: Add support for Optimizely experiments.
    Also upgrades to frontend-build 9.2.0
    ENT-5726

--- src/components/course/enrollment/components/ToDataSharingConsent.jsx
+++ src/components/course/enrollment/components/ToDataSharingConsent.jsx
@@ -3,15 +3,21 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Hyperlink } from '@edx/paragon';
 
-import { useTrackSearchConversionClickHandler } from '../../data/hooks';
+import {
+  useOptimizelyEnrollmentClickHandler,
+  useTrackSearchConversionClickHandler,
+} from '../../data/hooks';
 import { enrollLinkClass } from '../constants';
 import { EnrollButtonCta } from '../common';
 
 // Data sharing consent
 const ToDataSharingConsentPage = ({ enrollLabel, enrollmentUrl }) => {
-  const handleClick = useTrackSearchConversionClickHandler({
+  const analyticsHandler = useTrackSearchConversionClickHandler({
     href: enrollmentUrl,
     eventName: 'edx.ui.enterprise.learner_portal.course.enroll_button.to_dsc.clicked',
+  });
+  const optimizelyHandler = useOptimizelyEnrollmentClickHandler({
+    href: enrollmentUrl,
   });
 
   return (
@@ -20,7 +26,10 @@ const ToDataSharingConsentPage = ({ enrollLabel, enrollmentUrl }) => {
       as={Hyperlink}
       className={classNames('btn btn-primary btn-brand-primary d-block', enrollLinkClass)}
       destination={enrollmentUrl}
-      onClick={handleClick}
+      onClick={(e) => {
+        analyticsHandler(e);
+        optimizelyHandler(e);
+      }}
     />
   );
 };
---
+++ src/utils/optimizely.js
@@ -0,0 +1,59 @@
+export const EVENTS = {
+  ENROLLMENT_CLICK: 'enterprise_learner_portal_enrollment_click',
+};
+
+export const getActiveExperiments = () => {
+  if (!window.optimizely) {
+    return [];
+  }
+  return window.optimizely.get('state').getActiveExperimentIds();
+};
+
+export const getVariationMap = () => {
+  if (!window.optimizely) {
+    return false;
+  }
+  return window.optimizely.get('state').getVariationMap();
+};
+
+export const pushUserAttributes = (userAttributes) => {
+  if (!window.optimizely) {
+    return;
+  }
+  window.optimizely.push({
+    type: 'user',
+    attributes: userAttributes,
+  });
+};
+
+export const pushEvent = (eventName, eventMetadata) => {
+  if (!window.optimizely) {
+    return;
+  }
+  window.optimizely.push({
+    type: 'event',
+    eventName,
+    tags: eventMetadata,
+  });
+};
+
+export const pushUserCustomerAttributes = ({ uuid, slug }) => {
+  pushUserAttributes({
+    enterpriseCustomerUuid: uuid,
+    enterpriseCustomerSlug: slug,
+  });
+};
+
+export const pushEnrollmentClickEvent = (eventMetadata) => {
+  pushEvent(EVENTS.ENROLLMENT_CLICK, eventMetadata);
+};
+
+export const isExperimentActive = (experimentId) => getActiveExperiments().includes(experimentId);
+
+export const isExperimentVariant = (experimentId, variantId) => {
+  if (!isExperimentActive(experimentId)) {
+    return false;
+  }
+  const selectedVariant = getVariationMap()[experimentId];
+  return selectedVariant?.id === variantId;
+};
