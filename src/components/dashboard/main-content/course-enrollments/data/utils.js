546dd7f570baa4b03cc6a5cb11dd17ddc80333ea
Adam Stankiewicz <agstanki@gmail.com>
2022-04-29 12:55:23 -0400

    fix: handle null subsidyRequest.coursePartners (#513)

--- src/components/dashboard/main-content/course-enrollments/data/utils.js
+++ src/components/dashboard/main-content/course-enrollments/data/utils.js
@@ -56,7 +56,7 @@ export const transformSubsidyRequest = ({
 }) => ({
   courseRunId: subsidyRequest.courseId,
   title: subsidyRequest.courseTitle,
-  orgName: subsidyRequest.coursePartners.map(partner => partner.name).join(', '),
+  orgName: subsidyRequest.coursePartners?.map(partner => partner.name).join(', '),
   courseRunStatus: COURSE_STATUSES.requested,
   linkToCourse: `${slug}/course/${subsidyRequest.courseId}`,
   created: subsidyRequest.created,
