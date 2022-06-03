2cb990f9f7b007904042bfff212beeb5f6d80388
Long Lin <lin.longsheng01@gmail.com>
2022-06-03 15:09:40 -0400

    feat: update course pricing for enterprise offers

--- src/components/course/CourseSidebarPrice.jsx
+++ src/components/course/CourseSidebarPrice.jsx
@@ -9,20 +9,28 @@ import {
   useCoursePriceForUserSubsidy,
 } from './data/hooks';
 import { SubsidyRequestsContext } from '../enterprise-subsidy-requests';
-import { LICENSE_SUBSIDY_TYPE } from './data/constants';
+import { ENTERPRISE_OFFER_SUBSIDY_TYPE, LICENSE_SUBSIDY_TYPE } from './data/constants';
+import { UserSubsidyContext } from '../enterprise-user-subsidy/UserSubsidy';
+import ContactAdmin from '../contact-admin';
 
 export const INCLUDED_IN_SUBSCRIPTION_MESSAGE = 'Included in your subscription';
 export const FREE_WHEN_APPROVED_MESSAGE = 'Free to me\n(when approved)';
+export const COVERED_BY_ENTERPRISE_OFFER_MESSAGE = 'The price of this course could be covered with your organization\'s learning credit balance';
 
 const CourseSidebarPrice = () => {
+  const { enterpriseConfig } = useContext(AppContext);
   const { state: courseData } = useContext(CourseContext);
   const { activeCourseRun, userSubsidyApplicableToCourse } = courseData;
-  const { enterpriseConfig } = useContext(AppContext);
   const { subsidyRequestConfiguration } = useContext(SubsidyRequestsContext);
 
   const [coursePrice, currency] = useCoursePriceForUserSubsidy({
     activeCourseRun, userSubsidyApplicableToCourse,
   });
+
+  const {
+    enterpriseOffers,
+    canEnrollWithEnterpriseOffers,
+  } = useContext(UserSubsidyContext);
 
   if (!coursePrice) {
     return <Skeleton height={24} />;
@@ -64,8 +72,27 @@ const CourseSidebarPrice = () => {
 
   // Case 3: No subsidies found
   if (!hasDiscountedPrice) {
-    return <span>${originalPriceDisplay} {currency}</span>;
+    const { catalogList } = courseData.catalog;
+    const hasOfferWithInsufficientBalance = canEnrollWithEnterpriseOffers && enterpriseOffers.find(
+      (enterpriseOffer) => catalogList?.includes(enterpriseOffer.enterpriseCatalogUuid)
+       && enterpriseOffer.remainingBalance < coursePrice.list,
+    );
+
+    return (
+      <>
+        <span className="d-block">${originalPriceDisplay} {currency}</span>
+        {hasOfferWithInsufficientBalance && (
+          <small data-testid="insufficient-offer-balance-text">
+            Your organization doesn&apos;t have sufficient balance to cover this course.{' '}
+            {<ContactAdmin>Contact your administrator</ContactAdmin>} to learn more
+          </small>
+        )}
+      </>
+    );
   }
+
+  const discountedPriceMessage = userSubsidyApplicableToCourse?.subsidyType
+   === ENTERPRISE_OFFER_SUBSIDY_TYPE ? COVERED_BY_ENTERPRISE_OFFER_MESSAGE : `Sponsored by ${enterpriseConfig.name}`;
 
   // Case 4: subsidy found
   const discountedPriceDisplay = `${numberWithPrecision(coursePrice.discounted)} ${currency}`;
@@ -80,7 +107,7 @@ const CourseSidebarPrice = () => {
           </>
         )}
       </div>
-      <span>Sponsored by {enterpriseConfig.name}</span>
+      <small>{discountedPriceMessage}</small>
     </>
   );
 };
