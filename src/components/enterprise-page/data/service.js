802a26bed79097330e633043eca6fb467a6f9bcf
Long Lin <lin.longsheng01@gmail.com>
2022-07-13 17:37:51 -0400

    fix: update current active enterprise on page load

--- src/components/enterprise-page/data/service.js
+++ src/components/enterprise-page/data/service.js
@@ -1,11 +1,24 @@
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { getConfig } from '@edx/frontend-platform/config';
 
-export function fetchEnterpriseCustomerConfigForSlug(slug, useCache = true) {
+export const fetchEnterpriseCustomerConfigForSlug = (slug, useCache = true) => {
   const config = getConfig();
   const url = `${config.LMS_BASE_URL}/enterprise/api/v1/enterprise-customer/?slug=${slug}`;
   const httpClient = getAuthenticatedHttpClient({
     useCache: useCache && config.USE_API_CACHE,
   });
   return httpClient.get(url);
-}
+};
+
+export const updateUserActiveEnterprise = (enterpriseId) => {
+  const config = getConfig();
+  const url = `${config.LMS_BASE_URL}/enterprise/select/active`;
+
+  const formData = new FormData();
+  formData.append('enterprise', enterpriseId);
+
+  return getAuthenticatedHttpClient().post(
+    url,
+    formData,
+  );
+};
