f0dd570464b8e122f0e57fb90759cd2629012f70
Chris Pappas <cpappas@edx.org>
2022-07-18 13:59:25 -0400

    fix: tests

--- src/components/enterprise-user-subsidy/enterprise-offers/tests/EnterpriseOffersBalanceAlert.test.jsx
+++ src/components/enterprise-user-subsidy/enterprise-offers/tests/EnterpriseOffersBalanceAlert.test.jsx
@@ -2,27 +2,38 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import * as auth from '@edx/frontend-platform/auth';
 import { AppContext } from '@edx/frontend-platform/react';
 import { WarningFilled } from '@edx/paragon/icons';
 import EnterpriseOffersBalanceAlert from '../EnterpriseOffersBalanceAlert';
-import { LOW_BALANCE_ALERT_HEADING, LOW_BALANCE_ALERT_TEXT, LOW_BALANCE_CONTACT_ADMIN_TEXT } from '../data/constants';
+import {
+  LOW_BALANCE_ALERT_HEADING,
+  LOW_BALANCE_ALERT_TEXT,
+  LOW_BALANCE_CONTACT_ADMIN_TEXT,
+  NO_BALANCE_ALERT_TEXT,
+} from '../data/constants';
+
+const mockUser = {
+  userId: 1337,
+};
+
+jest.mock('@edx/frontend-platform/auth', () => ({
+  ...jest.requireActual('@edx/frontend-platform/auth'),
+  getAuthenticatedUser: () => ({ mockUser }),
+}));
 
 const EnterpriseOffersBalanceAlertWrapper = ({
   enterpriseConfig = {
     adminUsers: [],
   },
+  hasNoEnterpriseOffersBalance
 }) => (
   <AppContext.Provider value={{
     enterpriseConfig,
   }}
   >
     <EnterpriseOffersBalanceAlert
-      adminText={LOW_BALANCE_CONTACT_ADMIN_TEXT}
-      alertClassName="low-offers-balance-alert-with-cta"
-      alertVariant="warning"
-      alertIcon={WarningFilled}
-      alertHeading={LOW_BALANCE_ALERT_HEADING}
-      alertText={LOW_BALANCE_ALERT_TEXT}
+      hasNoEnterpriseOffersBalance={hasNoEnterpriseOffersBalance}
     />
   </AppContext.Provider>
 );
@@ -33,11 +44,28 @@ describe('<EnterpriseOffersBalanceAlert />', () => {
     expect(screen.queryByText(LOW_BALANCE_CONTACT_ADMIN_TEXT)).not.toBeInTheDocument();
   });
 
-  it('should render mailto link if there are enterprise admins', () => {
+  it('should render mailto link with no_balance text if there are enterprise admins', () => {
     const enterpriseConfig = {
       adminUsers: ['edx@example.org'],
     };
-    render(<EnterpriseOffersBalanceAlertWrapper enterpriseConfig={enterpriseConfig} />);
-    expect(screen.getByText(LOW_BALANCE_CONTACT_ADMIN_TEXT)).toBeInTheDocument();
+    render(
+      <EnterpriseOffersBalanceAlertWrapper
+        enterpriseConfig={enterpriseConfig}
+        hasNoEnterpriseOffersBalance={true}
+      />
+    );
+    expect(screen.getByText(NO_BALANCE_ALERT_TEXT)).toBeInTheDocument();
+  });
+
+  it('should render mailto link with low_balance text if there are enterprise admins', () => {
+    const enterpriseConfig = {
+      adminUsers: ['edx@example.org'],
+    };
+    render(<EnterpriseOffersBalanceAlertWrapper
+      enterpriseConfig={enterpriseConfig}
+      hasNoEnterpriseOffersBalance={false}
+      />
+    );
+    expect(screen.getByText(LOW_BALANCE_ALERT_TEXT)).toBeInTheDocument();
   });
 });
