0d1102ae49dc2a5fecc6aeb06b00976edb880bfb
Adam Stankiewicz <agstanki@gmail.com>
2022-07-19 15:05:50 -0400

    chore: add .eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+const { getBaseConfig } = require('@edx/frontend-build');
+const config = getBaseConfig('eslint');
+
+// Ignore linting on module.config.js
+config.ignorePatterns = ['module.config.js'];
+
+// Temporarily update the 'indent' and 'template-curly-spacing' rules
+// since they are causing eslint to fail for no apparent reason since
+// upgrading @edx/frontend-build from v3 to v5:
+//  - TypeError: Cannot read property 'range' of null
+config.rules['indent'] = ['error', 2, { 'ignoredNodes': ['TemplateLiteral', 'SwitchCase'] }];
+config.rules['template-curly-spacing'] = 'off';
+
+module.exports = config;
