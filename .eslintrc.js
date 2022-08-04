c8e76bab37da90606e79d2ea0a2f7612b963337e
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2022-08-04 13:15:20 +0500

    refactor: updated frontend-build version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 const { getBaseConfig } = require('@edx/frontend-build');
+
 const config = getBaseConfig('eslint');
 
 // Ignore linting on module.config.js
@@ -8,7 +9,7 @@ config.ignorePatterns = ['module.config.js'];
 // since they are causing eslint to fail for no apparent reason since
 // upgrading @edx/frontend-build from v3 to v5:
 //  - TypeError: Cannot read property 'range' of null
-config.rules['indent'] = ['error', 2, { 'ignoredNodes': ['TemplateLiteral', 'SwitchCase'] }];
+config.rules.indent = ['error', 2, { ignoredNodes: ['TemplateLiteral', 'SwitchCase'] }];
 config.rules['template-curly-spacing'] = 'off';
 config.rules['import/prefer-default-export'] = 'off';
 
