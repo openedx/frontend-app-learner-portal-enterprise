228be0b76eda74e913c7fe3d15d3b853998279e9
Adam Stankiewicz <agstanki@gmail.com>
2022-08-11 08:47:13 -0400

    fix: update tests and refactors

--- src/components/executive-education-2u/ExecutiveEducation2UPage.jsx
+++ src/components/executive-education-2u/ExecutiveEducation2UPage.jsx
@@ -1,5 +1,5 @@
 import React, {
-  useContext, useEffect, useState,
+  useContext, useEffect, useMemo,
 } from 'react';
 import { Container } from '@edx/paragon';
 import { AppContext } from '@edx/frontend-platform/react';
@@ -9,20 +9,31 @@ import { logError } from '@edx/frontend-platform/logging';
 
 import NotFoundPage from '../NotFoundPage';
 import UserEnrollmentForm from './UserEnrollmentForm';
-import { getExecutiveEducation2UContentMetadata, useActiveQueryParams } from './data';
+import {
+  useActiveQueryParams,
+  useExecutiveEducation2UContentMetadata,
+} from './data';
 
 function ExecutiveEducation2UPage() {
   const { enterpriseConfig } = useContext(AppContext);
   const activeQueryParams = useActiveQueryParams();
 
-  const [isLoadingContentMetadata, setIsLoadingContentMetadata] = useState(true);
-  const [contentMetadata, setContentMetadata] = useState();
+  const isExecEd2UFulfillmentEnabled = useMemo(() => (
+    enterpriseConfig.enableExecutiveEducation2UFulfillment && activeQueryParams.has('course_uuid')
+  ), [enterpriseConfig, activeQueryParams]);
 
-  const isExecEd2UFulfillmentEnabled = (enterpriseConfig.enableExecutiveEducation2UFulfillment && activeQueryParams.has('course_uuid'));
+  const {
+    isLoading: isLoadingContentMetadata,
+    contentMetadata,
+  } = useExecutiveEducation2UContentMetadata({
+    courseUUID: activeQueryParams.get('course_uuid'),
+    isExecEd2UFulfillmentEnabled,
+  });
 
   useEffect(() => {
     const isExecEd2UEnabled = enterpriseConfig.enableExecutiveEducation2UFulfillment;
     const hasCourseUUIDQueryParam = activeQueryParams.has('course_uuid');
+
     if (!isExecEd2UEnabled) {
       logError(`Enterprise ${enterpriseConfig.uuid} does not have executive education (2U) fulfillment enabled.`);
     }
@@ -30,24 +41,6 @@ function ExecutiveEducation2UPage() {
       logError(`Enterprise ${enterpriseConfig.uuid} visited ExecutiveEducation2UPage without required course_uuid query parameter.`);
     }
   }, [activeQueryParams, enterpriseConfig]);
-
-  useEffect(() => {
-    const fetchContentMetadata = async () => {
-      setIsLoadingContentMetadata(true);
-      try {
-        const courseUUID = activeQueryParams.get('course_uuid');
-        const metadata = await getExecutiveEducation2UContentMetadata(courseUUID);
-        setContentMetadata(metadata);
-      } catch (error) {
-        logError(error);
-      } finally {
-        setIsLoadingContentMetadata(false);
-      }
-    };
-    if (isExecEd2UFulfillmentEnabled) {
-      fetchContentMetadata();
-    }
-  }, [isExecEd2UFulfillmentEnabled, activeQueryParams]);
 
   if (!isExecEd2UFulfillmentEnabled) {
     return (
--- src/components/executive-education-2u/ExecutiveEducation2UPage.test.jsx
+++ src/components/executive-education-2u/ExecutiveEducation2UPage.test.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { AppContext } from '@edx/frontend-platform/react';
-import { logError } from '@edx/frontend-platform/logging';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { renderWithRouter } from '@edx/frontend-enterprise-utils';
 
 import ExecutiveEducation2UPage from './ExecutiveEducation2UPage';
-import { getContentMetadata, useActiveQueryParams } from './data';
+import {
+  useActiveQueryParams,
+  useExecutiveEducation2UContentMetadata,
+} from './data';
 
 jest.mock('./data');
 jest.mock('@edx/frontend-platform/logging', () => ({
@@ -42,7 +44,9 @@ describe('ExecutiveEducation2UPage', () => {
   });
 
   it('shows 404 when `enableExecutiveEducation2UFulfillment` is false', () => {
-    useActiveQueryParams.mockImplementation(() => new URLSearchParams());
+    useActiveQueryParams.mockReturnValue(new URLSearchParams());
+    useExecutiveEducation2UContentMetadata.mockReturnValue({ isLoading: false, contentMetadata: undefined });
+
     const appContextValue = {
       ...initialAppContextValue,
       enterpriseConfig: {
@@ -52,13 +56,14 @@ describe('ExecutiveEducation2UPage', () => {
     };
     renderWithRouter(<ExecutiveEducation2UPageWrapper appContextValue={appContextValue} />);
     expect(screen.getByText('404')).toBeInTheDocument();
-
-    expect(getContentMetadata).not.toHaveBeenCalled();
   });
 
   it('does not render page when `enableExecutiveEducation2UFulfillment` is true and course_uuid is not provided', async () => {
     const courseTitle = 'edX Demonstration Course';
-    getContentMetadata.mockResolvedValueOnce({ title: courseTitle });
+    useExecutiveEducation2UContentMetadata.mockReturnValue({
+      isLoading: false,
+      contentMetadata: { title: courseTitle },
+    });
     const searchParams = new URLSearchParams();
     useActiveQueryParams.mockImplementation(() => searchParams);
 
@@ -66,42 +71,37 @@ describe('ExecutiveEducation2UPage', () => {
     expect(screen.queryByText('404')).toBeInTheDocument();
   });
 
-  it('renders page when `enableExecutiveEducation2UFulfillment` is true and course_uuid is provided', async () => {
+  it('renders page when `enableExecutiveEducation2UFulfillment` is true and course_uuid is provided', () => {
     const courseTitle = 'edX Demonstration Course';
-    getContentMetadata.mockResolvedValueOnce({ title: courseTitle });
+    useExecutiveEducation2UContentMetadata.mockReturnValue({
+      isLoading: false,
+      contentMetadata: { title: courseTitle },
+    });
     const searchParams = new URLSearchParams({ course_uuid: 'test-course-uuid' });
     useActiveQueryParams.mockImplementation(() => searchParams);
 
     renderWithRouter(<ExecutiveEducation2UPageWrapper />);
     expect(screen.queryByText('404')).not.toBeInTheDocument();
 
-    // initially in loading state
-    expect(screen.getByTestId('loading-skeleton-page-title')).toBeInTheDocument();
-    expect(screen.getByTestId('loading-skeleton-text-blurb')).toBeInTheDocument();
-
-    // no longer in loading state
-    await waitFor(() => {
-      expect(screen.queryByTestId('loading-skeleton-page-title')).not.toBeInTheDocument();
-      expect(screen.queryByTestId('loading-skeleton-text-blurb')).not.toBeInTheDocument();
-    });
+    expect(screen.queryByTestId('loading-skeleton-page-title')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('loading-skeleton-text-blurb')).not.toBeInTheDocument();
 
     expect(screen.getByText(courseTitle, { exact: false })).toBeInTheDocument();
     expect(screen.getByTestId('user-enrollment-form-component')).toBeInTheDocument();
   });
 
-  it('handles exception when fetching content metadata', async () => {
+  it('renders page with loading states when fetching content metadata', () => {
+    useExecutiveEducation2UContentMetadata.mockReturnValue({
+      isLoading: true,
+      contentMetadata: undefined,
+    });
     const searchParams = new URLSearchParams({ course_uuid: 'test-course-uuid' });
     useActiveQueryParams.mockImplementation(() => searchParams);
-    const mockError = new Error('oh noes');
-    getContentMetadata.mockImplementation(() => new Promise(() => { throw mockError; }));
 
     renderWithRouter(<ExecutiveEducation2UPageWrapper />);
     expect(screen.queryByText('404')).not.toBeInTheDocument();
-    expect(screen.getByTestId('loading-skeleton-text-blurb')).toBeInTheDocument();
 
-    await waitFor(() => {
-      expect(logError).toHaveBeenCalledTimes(1);
-      expect(logError).toHaveBeenCalledWith(mockError);
-    });
+    expect(screen.getByTestId('loading-skeleton-page-title')).toBeInTheDocument();
+    expect(screen.getByTestId('loading-skeleton-text-blurb')).toBeInTheDocument();
   });
 });
