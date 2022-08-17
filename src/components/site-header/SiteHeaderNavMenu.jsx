6d086406c5d7cb07f723a9d352db1ff1d2f45a53
Long Lin <lin.longsheng01@gmail.com>
2022-08-17 12:06:19 -0400

    feat: add getsmarter logo to exec ed page

---
+++ src/components/site-header/SiteHeaderNavMenu.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AppContext } from '@edx/frontend-platform/react';
+
+const SiteHeaderNavMenu = () => {
+  const { enterpriseConfig } = useContext(AppContext);
+  const mainMenuLinkClassName = 'nav-link';
+
+  if (enterpriseConfig.disableSearch) {
+    return null;
+  }
+
+  return (
+    <>
+      <NavLink to={`/${enterpriseConfig.slug}`} className={mainMenuLinkClassName} exact>
+        Dashboard
+      </NavLink>
+      <NavLink to={`/${enterpriseConfig.slug}/search`} className={mainMenuLinkClassName} exact>
+        Find a Course
+      </NavLink>
+    </>
+  );
+};
+
+export default SiteHeaderNavMenu;
