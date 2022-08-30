c629435b5d0513c3871d80091591f73741588d28
Muneeb Ur Rehman <muneeb.github@gmail.com>
2022-08-30 17:50:44 +0500

    style: reduce gap between progress categories

--- src/components/progress-category-bubbles/ProgressCategoryBubbles.jsx
+++ src/components/progress-category-bubbles/ProgressCategoryBubbles.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import './styles/index.scss';
 
 const ProgressCategoryBubbles = ({ notStarted, inProgress, completed }) => (
-  <Stack direction="horizontal" gap={2.5}>
+  <Stack direction="horizontal" gap={2}>
     <Bubble className="remaining-courses" data-testid="remaining-count">
       {notStarted}
     </Bubble>
