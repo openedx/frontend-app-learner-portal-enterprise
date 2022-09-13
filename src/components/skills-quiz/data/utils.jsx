818eb2c489a13902e1b4a0ed61cef0ccb9d52c89
zamanafzal <zamanafzal@gmail.com>
2022-09-14 02:02:30 +0500

    feat: Added ability to save user selected options on skill quiz

--- src/components/skills-quiz/data/utils.jsx
+++ src/components/skills-quiz/data/utils.jsx
@@ -3,11 +3,38 @@
 import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
 import { sendEnterpriseTrackEvent } from '@edx/frontend-enterprise-utils';
 
+import { fetchSkillsId, postSkillsGoalsAndJobsUserSelected } from './service';
+import {
+  DROPDOWN_OPTION_CHANGE_CAREERS,
+  DROPDOWN_OPTION_CHANGE_CAREERS_LABEL,
+  DROPDOWN_OPTION_GET_PROMOTED,
+  DROPDOWN_OPTION_GET_PROMOTED_LABEL,
+  DROPDOWN_OPTION_IMPROVE_CURRENT_ROLE,
+  DROPDOWN_OPTION_IMPROVE_CURRENT_ROLE_LABEL,
+  DROPDOWN_OPTION_OTHER,
+  DROPDOWN_OPTION_OTHER_LABEL,
+} from '../constants';
+
 export default function getCommonSkills(content, selectedJobSkills, MAX_VISIBLE_SKILLS) {
   const contentSkills = content.skillNames || [];
   return contentSkills.filter(skill => selectedJobSkills.includes(skill)).slice(0, MAX_VISIBLE_SKILLS);
 }
 
+export const goalLabels = (goal) => {
+  if (goal === DROPDOWN_OPTION_CHANGE_CAREERS) {
+    return DROPDOWN_OPTION_CHANGE_CAREERS_LABEL;
+  }
+  if (goal === DROPDOWN_OPTION_GET_PROMOTED) {
+    return DROPDOWN_OPTION_GET_PROMOTED_LABEL;
+  }
+  if (goal === DROPDOWN_OPTION_IMPROVE_CURRENT_ROLE) {
+    return DROPDOWN_OPTION_IMPROVE_CURRENT_ROLE_LABEL;
+  }
+  if (goal === DROPDOWN_OPTION_OTHER) {
+    return DROPDOWN_OPTION_OTHER_LABEL;
+  }
+  return DROPDOWN_OPTION_OTHER_LABEL;
+};
 export function sortSkillsWithSignificance(job) {
   return job?.skills?.sort((a, b) => (
     (a.significance < b.significance) ? 1 : -1));
@@ -36,3 +63,12 @@ export function linkToCourse(course, slug, enterpriseUUID) {
   );
   return `/${slug}/course/${course.key}?${queryParams.toString()}`;
 }
+
+export const saveSkillsGoalsAndJobsUserSelected = async (goal, skills, currentJobRole, interestedJobs) => {
+  const { data: { results } } = await fetchSkillsId(skills);
+  const skillsId = results?.map(({ id }) => id);
+  const interestedJobsId = interestedJobs?.map(({ id }) => id);
+  const currentJobRoleId = currentJobRole?.map(({ id }) => id);
+  const goalLabel = goalLabels(goal);
+  postSkillsGoalsAndJobsUserSelected(goalLabel, skillsId, interestedJobsId, currentJobRoleId);
+};
