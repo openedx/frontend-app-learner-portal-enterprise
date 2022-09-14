8ad9f9383d41c1baf9e9c6af8f65126bc3c44587
Alex Dusenbery <adusenbery@edx.org>
2022-09-14 09:49:00 -0400

    feat: add a trackingName to SearchData instance, so that queries are tracked.
    temporarily unblock PRs with failing codecov

--- src/components/search/SearchPage.jsx
+++ src/components/search/SearchPage.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { SearchData } from '@edx/frontend-enterprise-catalog-search';
 
 import Search from './Search';
-import { SEARCH_FACET_FILTERS } from './constants';
+import { SEARCH_FACET_FILTERS, SEARCH_TRACKING_NAME } from './constants';
 
 const SearchPage = () => (
-  <SearchData searchFacetFilters={SEARCH_FACET_FILTERS}>
+  <SearchData searchFacetFilters={SEARCH_FACET_FILTERS} trackingName={SEARCH_TRACKING_NAME}>
     <Search />
   </SearchData>
 );
