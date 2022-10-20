a882817ca402289cd848981d96d60d3bb22be9d6
Hamzah Ullah <hamzahullah@yahoo.com>
2022-10-20 08:23:21 -0400

    feat: Added enterpriseUUID value through to catalog-search package for segment tracking

--- src/components/search/SearchPage.jsx
+++ src/components/search/SearchPage.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SearchData } from '@edx/frontend-enterprise-catalog-search';
-
+import { AppContext } from '@edx/frontend-platform/react';
 import Search from './Search';
 import { SEARCH_FACET_FILTERS, SEARCH_TRACKING_NAME } from './constants';
 
-const SearchPage = () => (
-  <SearchData searchFacetFilters={SEARCH_FACET_FILTERS} trackingName={SEARCH_TRACKING_NAME}>
-    <Search />
-  </SearchData>
-);
+const SearchPage = () => {
+  const { enterpriseConfig } = useContext(AppContext);
+  return (
+    <SearchData
+      searchFacetFilters={SEARCH_FACET_FILTERS}
+      trackingName={SEARCH_TRACKING_NAME}
+      enterpriseUUID={enterpriseConfig.uuid}
+    >
+      <Search />
+    </SearchData>
+  );
+};
 
 export default SearchPage;
