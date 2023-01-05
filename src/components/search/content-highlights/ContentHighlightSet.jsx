d703e2f897cfc5752be5ac86a4fabc76fa84dcd7
Adam Stankiewicz <agstanki@gmail.com>
2023-01-04 22:55:10 -0500

    fix: remove temp code

--- src/components/search/content-highlights/ContentHighlightSet.jsx
+++ src/components/search/content-highlights/ContentHighlightSet.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import {
@@ -8,23 +8,19 @@ import {
   useMediaQuery,
   breakpoints,
 } from '@edx/paragon';
+import { sendEnterpriseTrackEvent } from '@edx/frontend-enterprise-utils';
+import { AppContext } from '@edx/frontend-platform/react';
 
 import { CARDGRID_COLUMN_SIZES } from '../constants';
 import HighlightedContentCard from './HighlightedContentCard';
 
 const ContentHighlightSet = ({ highlightSet }) => {
+  const { enterpriseConfig: { uuid: enterpriseUUID } } = useContext(AppContext);
   const { title } = highlightSet;
 
-  const highlightedContent = useMemo(() => {
-    const content = [
-      ...highlightSet.highlightedContent,
-      ...highlightSet.highlightedContent,
-      ...highlightSet.highlightedContent,
-    ].map(highlightedContentItem => (
-      <HighlightedContentCard key={uuidv4()} highlightedContent={highlightedContentItem} />
-    ));
-    return content;
-  }, [highlightSet]);
+  const highlightedContent = useMemo(() => highlightSet.highlightedContent.map(highlightedContentItem => (
+    <HighlightedContentCard key={uuidv4()} highlightedContent={highlightedContentItem} />
+  )), [highlightSet]);
 
   const isMobileWindowSize = useMediaQuery({
     query: `(max-width: ${breakpoints.medium.maxWidth}px)`,
@@ -38,10 +34,16 @@ const ContentHighlightSet = ({ highlightSet }) => {
         subtitle="Enroll in content selected for you by your organization."
         columnSizes={CARDGRID_COLUMN_SIZES}
         onScrollPrevious={() => {
-          // TODO: add segment event
+          sendEnterpriseTrackEvent(
+            enterpriseUUID,
+            'edx.ui.enterprise.learner_portal.search.content_highlights.card_carousel.scrolled_to_previous',
+          );
         }}
         onScrollNext={() => {
-          // TODO: add segment event
+          sendEnterpriseTrackEvent(
+            enterpriseUUID,
+            'edx.ui.enterprise.learner_portal.search.content_highlights.card_carousel.scrolled_to_next',
+          );
         }}
         CardCarouselControls={isMobileWindowSize ? () => null : undefined}
         canScrollHorizontal={isMobileWindowSize}
@@ -68,14 +70,20 @@ ContentHighlightSet.propTypes = {
   }).isRequired,
 };
 
-const ContentHighlightSetSkeleton = () => (
-  <>
-    <h2 className="mb-3"><Skeleton /></h2>
-    <CardDeck columnSizes={CARDGRID_COLUMN_SIZES}>
-      {Array.from({ length: 4 }).map(() => <HighlightedContentCard.Skeleton key={uuidv4()} />)}
-    </CardDeck>
-  </>
-);
+const ContentHighlightSetSkeleton = () => {
+  const skeletonCards = useMemo(() => Array.from({ length: 4 }).map(() => (
+    <HighlightedContentCard.Skeleton key={uuidv4()} />
+  )), []);
+
+  return (
+    <>
+      <h2 className="mb-3"><Skeleton /></h2>
+      <CardDeck columnSizes={CARDGRID_COLUMN_SIZES}>
+        {skeletonCards}
+      </CardDeck>
+    </>
+  );
+};
 
 ContentHighlightSet.Skeleton = ContentHighlightSetSkeleton;
 
