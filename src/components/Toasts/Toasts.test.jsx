5bc6f3a036109b19c18409f4a1645fb6395290e2
Hamzah Ullah <hamzahullah@yahoo.com>
2023-01-18 12:45:03 -0500

    chore: cleaned up toast test

--- src/components/Toasts/Toasts.test.jsx
+++ src/components/Toasts/Toasts.test.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import {
+  render, screen, act, fireEvent,
+} from '@testing-library/react';
+import { IntlProvider } from '@edx/frontend-platform/i18n';
 import ToastsProvider, { ToastsContext } from './ToastsProvider';
+import Toasts from './Toasts';
 
 describe('ToastsProvider', () => {
   it('should add and remove toasts', () => {
     let toastsContext;
     render(
-      <ToastsProvider>
-        <ToastsContext.Consumer>
-          {(context) => {
-            toastsContext = context;
-            return <div />;
-          }}
-        </ToastsContext.Consumer>
-      </ToastsProvider>,
+      <IntlProvider locale="en">
+        <ToastsProvider>
+          <ToastsContext.Consumer>
+            {(context) => {
+              toastsContext = context;
+              return <Toasts />;
+            }}
+          </ToastsContext.Consumer>
+        </ToastsProvider>,
+      </IntlProvider>,
     );
     expect(toastsContext.toasts).toEqual([]);
     act(() => {
@@ -21,9 +27,41 @@ describe('ToastsProvider', () => {
     });
     expect(toastsContext.toasts.length).toBe(1);
     expect(toastsContext.toasts[0].message).toBe('Hello World');
+    expect(screen.getByText('Hello World')).toBeTruthy();
     act(() => {
       toastsContext.removeToast(0);
     });
     expect(toastsContext.toasts.length).toBe(0);
+    expect(screen.queryByText('Hello World')).toBeFalsy();
+  });
+  it('should remove toasts when the user clicks', () => {
+    let toastsContext;
+    render(
+      <IntlProvider locale="en">
+        <ToastsProvider>
+          <ToastsContext.Consumer>
+            {(context) => {
+              toastsContext = context;
+              return <Toasts />;
+            }}
+          </ToastsContext.Consumer>
+        </ToastsProvider>,
+      </IntlProvider>,
+    );
+    act(() => {
+      toastsContext.addToast('Hello World');
+    });
+    expect(toastsContext.toasts.length).toBe(1);
+    expect(toastsContext.toasts[0].message).toBe('Hello World');
+    expect(screen.getByText('Hello World')).toBeTruthy();
+    const closeButton = screen.getByLabelText('Close');
+    const toastContainerClassesBefore = screen.getAllByRole('alert')[1].className;
+
+    expect(toastContainerClassesBefore.match(/show/)).toBeTruthy();
+
+    fireEvent.click(closeButton);
+    const toastContainerClassesAfter = screen.getAllByRole('alert')[0].className;
+
+    expect(toastContainerClassesAfter.match(/show/)).toBeFalsy();
   });
 });
