c31651e669860b46bd4f0cc3580d48bbc3475c9b
Hamzah Ullah <hamzahullah@yahoo.com>
2023-03-08 12:36:08 -0500

    fix: Adds additional conditional for contentType

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -92,8 +92,7 @@ const Search = () => {
   const shouldDisplayBalanceAlert = hasNoEnterpriseOffersBalance || hasLowEnterpriseOffersBalance;
 
   const { content_type: contentType } = refinements;
-  const hasRefinements = Object.keys(refinements).filter(refinement => refinement !== 'showAll').length > 0;
-
+  const hasRefinements = Object.keys(refinements).filter(refinement => refinement !== 'showAll').length > 0 && contentType?.length > 0;
   return (
     <>
       <Helmet title={PAGE_TITLE} />
