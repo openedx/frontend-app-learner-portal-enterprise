4ef4f42cd732d4b57a48859f6611bb76188b20a2
Adam Stankiewicz <agstanki@gmail.com>
2023-04-08 18:19:30 -0400

    fix: return mock redeemable access policy if ENABLE_EMET_REDEMPTION enabled via query param

--- src/components/course/data/utils.jsx
+++ src/components/course/data/utils.jsx
@@ -347,14 +347,20 @@ export const createEnrollWithCouponCodeUrl = ({
  * @returns
  */
 export const checkPolicyRedemptionEnabled = ({
-  accessPolicyRedemptionEligibilityData = {},
+  accessPolicyRedemptionEligibilityData = [],
 }) => {
   if (hasFeatureFlagEnabled('ENABLE_EMET_REDEMPTION')) {
     // Always enable the policy redemption feature when enabled via query parameter.
     return true;
   }
-  const { canRedeem: canRedeemAccessPolicy } = accessPolicyRedemptionEligibilityData;
+  const canRedeemAccessPolicy = accessPolicyRedemptionEligibilityData.some(({ canRedeem }) => canRedeem);
   const isFeatureEnabled = getConfig().FEATURE_ENABLE_EMET_REDEMPTION;
+
+  console.log('[checkPolicyRedemptionEnabled]', {
+    accessPolicyRedemptionEligibilityData,
+    isFeatureEnabled,
+    canRedeemAccessPolicy,
+  });
 
   // Enable EMET access policy redemption when the feature is enabled and there is a redeemable access policy.
   if (isFeatureEnabled && canRedeemAccessPolicy) {
