c071342004fe887163bd0699c6ced0bf6fede3e4
Adam Stankiewicz <agstanki@gmail.com>
2023-04-18 18:19:03 -0400

    Revert "feat: include offers with per-user limits (#712)"
    This reverts commit d5aab048e8cb78a9da121bb4648ab6fd3c8bf85f.

--- src/components/enterprise-user-subsidy/enterprise-offers/data/service.js
+++ src/components/enterprise-user-subsidy/enterprise-offers/data/service.js
@@ -7,6 +7,8 @@ export function fetchEnterpriseOffers(enterpriseId, options = {
   discount_value: 100,
   status: ENTERPRISE_OFFER_STATUS.OPEN,
   is_current: true,
+  max_user_applications__isnull: true, // We won't handle offers with per user limits for MVP
+  max_user_discount__isnull: true, // Remove filter when we're ready to support per user limit
 }) {
   const queryParams = new URLSearchParams({
     ...options,
