c37b264f1a1fca366b826ac728c228cc938420a9
Adam Stankiewicz <agstanki@gmail.com>
2023-04-19 10:41:02 -0400

    fix: invalidate can-user-redeem-course query cache upon successful redemption

--- src/components/course/course-header/data/hooks/useRedemptionStatus.js
+++ src/components/course/course-header/data/hooks/useRedemptionStatus.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
 
 /**
  * TODO
  * @returns
  */
 const useRedemptionStatus = () => {
+  const queryClient = useQueryClient();
   const [redemptionStatus, setRedemptionStatus] = useState();
 
   const handleRedeemClick = () => {
@@ -12,7 +14,10 @@ const useRedemptionStatus = () => {
   };
 
   const handleRedeemSuccess = (transaction) => {
+    queryClient.invalidateQueries({ queryKey: ['can-user-redeem-course'] });
     setRedemptionStatus('success');
+
+    // redirect to courseware
     const { coursewareUrl } = transaction;
     global.location.assign(coursewareUrl);
   };
