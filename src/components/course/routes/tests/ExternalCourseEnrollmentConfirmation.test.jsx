7a645236620806fd2e584b2222a73e6fc0a56ae2
Adam Stankiewicz <agstanki@gmail.com>
2023-06-07 07:57:51 -0400

    chore: add another test

--- src/components/course/routes/tests/ExternalCourseEnrollmentConfirmation.test.jsx
+++ src/components/course/routes/tests/ExternalCourseEnrollmentConfirmation.test.jsx
@@ -100,4 +100,26 @@ describe('ExternalCourseEnrollment', () => {
     expect(screen.getByText('Something went wrong.')).toBeInTheDocument();
     expect(screen.getByText('No learner credit is available to cover this course.'));
   });
+
+  it('handles successful prior redemption', () => {
+    const courseContextValue = {
+      ...baseCourseContextValue,
+      userSubsidyApplicableToCourse: undefined,
+      hasSuccessfulRedemption: true,
+      missingUserSubsidyReason: { reason: DISABLED_ENROLL_REASON_TYPES.NO_SUBSIDY_NO_ADMINS },
+    };
+    render(<ExternalCourseEnrollmentConfirmationWrapper courseContextValue={courseContextValue} />);
+    expect(screen.getByText('Congratulations, you have completed your enrollment for your online course')).toBeInTheDocument();
+    expect(screen.getByText('Test Course Title')).toBeInTheDocument();
+    expect(screen.getByText('Test Org')).toBeInTheDocument();
+    expect(screen.getByText('Start date:')).toBeInTheDocument();
+    expect(screen.getByText('March 5, 2023')).toBeInTheDocument();
+    expect(screen.getByText('Course duration:')).toBeInTheDocument();
+    expect(screen.getByText('3 Weeks')).toBeInTheDocument();
+    expect(screen.getByText('Course total:')).toBeInTheDocument();
+    expect(screen.getByText('$100.00 USD')).toBeInTheDocument();
+    expect(screen.getByText('What happens next?')).toBeInTheDocument();
+    expect(screen.getByText('Terms and Conditions')).toBeInTheDocument();
+    expect(screen.getByText('Terms and Conditions')).toBeInTheDocument();
+  });
 });
