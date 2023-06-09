e1ca7ae67df7cfd5ee2a9b5d9bb21c5e37fbdbf5
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2023-06-09 11:21:37 +0500

    feat: update react & react-dom to v17

--- src/setupTest.js
+++ src/setupTest.js
@@ -6,7 +6,7 @@ import ResizeObserverPolyfill from 'resize-observer-polyfill';
 import 'jest-canvas-mock';
 
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
 
