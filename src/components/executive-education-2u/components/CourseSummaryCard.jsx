ff78c28dcd90fb8430442db448890cde50049708
Hamzah Ullah <hamzahullah@yahoo.com>
2023-06-14 15:16:22 -0400

    feat: adds strikethrough on price for exec ed

--- src/components/executive-education-2u/components/CourseSummaryCard.jsx
+++ src/components/executive-education-2u/components/CourseSummaryCard.jsx
@@ -8,50 +8,60 @@ import {
 import { numberWithPrecision } from '../../course/data/utils';
 import { DATE_FORMAT } from '../../course/data/constants';
 
-const CourseSummaryCard = ({ courseMetadata, enrollmentCompleted }) => (
-  <Card
-    className="mb-4 course-summary"
-    orientation="horizontal"
-  >
-    <Card.Body>
-      <Card.Header
-        title={<Image src={courseMetadata.organizationImage} />}
-      />
-      <Card.Section>
-        <Row>
-          <Col xs={12} lg={{ span: 8, offset: 0 }} className="vertical">
-            <Row>
-              <Col xs={12} lg={{ span: 8, offset: 0 }}>
-                <p className="small font-weight-light text-gray-500">{courseMetadata.organizationName}</p>
-                <p>{courseMetadata.title}</p>
-              </Col>
-            </Row>
-          </Col>
-          <Col xs={12} lg={4}>
-            <div className="course-details">
-              <Row className="align-items-center">
-                <Col className="small font-weight-light text-gray-500 justify-content-start">{enrollmentCompleted ? 'Start date:' : 'Available start date:'}</Col>
-                <Col className="justify-content-end"><Row className="justify-content-end mr-2.5">{moment(courseMetadata.startDate).format(DATE_FORMAT)}</Row></Col>
-              </Row>
-              <Row className="align-items-center">
-                <Col className="small font-weight-light text-gray-500 justify-content-start">Course duration:</Col>
-                <Col className="justify-content-end"><Row className="justify-content-end mr-2.5">{courseMetadata.duration}</Row></Col>
-              </Row>
-              <Row className="align-items-center">
-                <Col className="small font-weight-light text-gray-500 justify-content-start">Course total:</Col>
-                <Col className="justify-content-end">
-                  <Row className="justify-content-end mr-2.5">
-                    {courseMetadata.priceDetails?.price ? `$${numberWithPrecision(courseMetadata.priceDetails.price)} ${courseMetadata.priceDetails.currency}` : '-'}
-                  </Row>
+const CourseSummaryCard = ({ courseMetadata, enrollmentCompleted }) => {
+  let coursePrice = null;
+  const precisePrice = courseMetadata?.priceDetails?.price ? `$${numberWithPrecision(courseMetadata.priceDetails.price)} ${courseMetadata.priceDetails.currency}` : '-';
+  if (enrollmentCompleted && courseMetadata?.priceDetails?.price) {
+    coursePrice = (<del>{precisePrice}</del>);
+  } else {
+    coursePrice = precisePrice;
+  }
+
+  return (
+    <Card
+      className="mb-4 course-summary"
+      orientation="horizontal"
+    >
+      <Card.Body>
+        <Card.Header
+          title={<Image src={courseMetadata.organizationImage} />}
+        />
+        <Card.Section>
+          <Row>
+            <Col xs={12} lg={{ span: 8, offset: 0 }} className="vertical">
+              <Row>
+                <Col xs={12} lg={{ span: 8, offset: 0 }}>
+                  <p className="small font-weight-light text-gray-500">{courseMetadata.organizationName}</p>
+                  <p>{courseMetadata.title}</p>
                 </Col>
               </Row>
-            </div>
-          </Col>
-        </Row>
-      </Card.Section>
-    </Card.Body>
-  </Card>
-);
+            </Col>
+            <Col xs={12} lg={4}>
+              <div className="course-details">
+                <Row className="align-items-center">
+                  <Col className="small font-weight-light text-gray-500 justify-content-start">{enrollmentCompleted ? 'Start date:' : 'Available start date:'}</Col>
+                  <Col className="justify-content-end"><Row className="justify-content-end mr-2.5">{moment(courseMetadata.startDate).format(DATE_FORMAT)}</Row></Col>
+                </Row>
+                <Row className="align-items-center">
+                  <Col className="small font-weight-light text-gray-500 justify-content-start">Course duration:</Col>
+                  <Col className="justify-content-end"><Row className="justify-content-end mr-2.5">{courseMetadata.duration}</Row></Col>
+                </Row>
+                <Row className="align-items-center">
+                  <Col className="small font-weight-light text-gray-500 justify-content-start">Course total:</Col>
+                  <Col className="justify-content-end">
+                    <Row className="justify-content-end mr-2.5">
+                      {coursePrice}
+                    </Row>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </Card.Section>
+      </Card.Body>
+    </Card>
+  );
+};
 
 CourseSummaryCard.defaultProps = {
   enrollmentCompleted: false,
