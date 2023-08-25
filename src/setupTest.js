7b2ba1b4afdc469fd001a1df890bcfa2b543fc7f
Adam Stankiewicz <agstanki@gmail.com>
2023-08-25 10:59:01 -0400

    fix: replicate admin-portal process.env.TZ

--- src/setupTest.js
+++ src/setupTest.js
@@ -10,8 +10,6 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-process.env.TZ = 'UTC';
-
 process.env.LMS_BASE_URL = 'http://localhost:18000';
 process.env.ECOMMERCE_BASE_URL = 'http://ecommerce.url';
 process.env.MARKETING_SITE_BASE_URL = 'http://marketing.url';
