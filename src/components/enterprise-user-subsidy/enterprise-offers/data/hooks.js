17bab96931cc179cc3b742cc44329c575eb41ba5
Adam Stankiewicz <agstanki@gmail.com>
2023-09-15 11:33:57 -0400

    fix: ensure course page can still handle expired offers

--- src/components/enterprise-user-subsidy/enterprise-offers/data/hooks.js
+++ src/components/enterprise-user-subsidy/enterprise-offers/data/hooks.js
@@ -46,12 +46,14 @@ export const useEnterpriseOffers = ({
       return;
     }
 
+    if (enterpriseOffers.length > 0) {
+      setCanEnrollWithEnterpriseOffers(true);
+    }
+
     const currentEnterpriseOffers = enterpriseOffers.filter(offer => offer.isCurrent);
     if (currentEnterpriseOffers.length > 0) {
       const hasLowBalance = currentEnterpriseOffers.some(offer => offer.isLowOnBalance);
       const hasNoBalance = currentEnterpriseOffers.every(offer => offer.isOutOfBalance);
-
-      setCanEnrollWithEnterpriseOffers(true);
       setHasLowEnterpriseOffersBalance(hasLowBalance);
       setHasNoEnterpriseOffersBalance(hasNoBalance);
     }
