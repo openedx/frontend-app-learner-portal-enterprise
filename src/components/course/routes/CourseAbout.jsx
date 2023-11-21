d4e36fff18540a8ef36f895ec65cb7daf10976e8
jajjibhai008 <ejazofficial122@gmail.com>
2023-11-21 16:25:00 +0500

    feat: hide backdoors for search course

--- src/components/course/routes/CourseAbout.jsx
+++ src/components/course/routes/CourseAbout.jsx
@@ -4,16 +4,45 @@ import {
 } from '@edx/paragon';
 
 import { AppContext } from '@edx/frontend-platform/react';
+import { Redirect } from 'react-router-dom';
 import { MainContent, Sidebar } from '../../layout';
 import CourseHeader from '../course-header/CourseHeader';
 import CourseMainContent from '../CourseMainContent';
 import CourseSidebar from '../CourseSidebar';
 import CourseRecommendations from '../CourseRecommendations';
 import { CourseContext } from '../CourseContextProvider';
+import { UserSubsidyContext } from '../../enterprise-user-subsidy';
+import { isDisableCourseSearch } from '../../enterprise-user-subsidy/enterprise-offers/data/utils';
+import { useIsCourseAssigned } from '../data/hooks';
+import { features } from '../../../config';
 
 const CourseAbout = () => {
-  const { canOnlyViewHighlightSets } = useContext(CourseContext);
+  const {
+    canOnlyViewHighlightSets,
+    state: {
+      course,
+    },
+  } = useContext(CourseContext);
   const { enterpriseConfig } = useContext(AppContext);
+  const {
+    redeemableLearnerCreditPolicies,
+    enterpriseOffers,
+    subscriptionPlan,
+    subscriptionLicense,
+  } = useContext(UserSubsidyContext);
+
+  const isCourseAssigned = useIsCourseAssigned(redeemableLearnerCreditPolicies, course?.key);
+  const hideCourseSearch = isDisableCourseSearch(
+    redeemableLearnerCreditPolicies,
+    enterpriseOffers,
+    subscriptionPlan,
+    subscriptionLicense,
+  );
+
+  const featuredHideCourseSearch = features.FEATURE_ENABLE_TOP_DOWN_ASSIGNMENT && hideCourseSearch;
+  if (!isCourseAssigned && !featuredHideCourseSearch) {
+    return <Redirect to={`/${enterpriseConfig.slug}`} />;
+  }
   return (
     <>
       <CourseHeader />
@@ -29,7 +58,8 @@ const CourseAbout = () => {
               </Sidebar>
             )}
           </MediaQuery>
-          {(canOnlyViewHighlightSets === false && !enterpriseConfig.disableSearch) && <CourseRecommendations />}
+          {(canOnlyViewHighlightSets === false
+            && !enterpriseConfig.disableSearch && !featuredHideCourseSearch) && <CourseRecommendations />}
         </Row>
       </Container>
     </>
