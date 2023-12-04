0ceba9a4ad14c0cd0eae13e62dd3a2ee1337781f
Hamzah Ullah <hamzahullah@yahoo.com>
2023-12-04 17:28:08 -0500

    fix: added failsafe default value for undefined 'assignments'

--- src/components/dashboard/data/utils.js
+++ src/components/dashboard/data/utils.js
@@ -6,8 +6,8 @@ import { ASSIGNMENT_TYPES } from '../../enterprise-user-subsidy/enterprise-offer
  * @param assignments - flatMap'ed object from redeemableLearnerCreditPolicies for learnerContentAssignments
  * @returns {{hasActiveAssignments: boolean, activeAssignments: Array}}
  */
-export default function getActiveAssignments(assignments) {
-  const activeAssignments = assignments?.filter((assignment) => [
+export default function getActiveAssignments(assignments = []) {
+  const activeAssignments = assignments.filter((assignment) => [
     ASSIGNMENT_TYPES.CANCELLED, ASSIGNMENT_TYPES.ALLOCATED,
   ].includes(assignment.state));
   const hasActiveAssignments = activeAssignments.length > 0;
