02c37d033c0aaf81fd88afa1981626b912beafaa
Hamzah Ullah <hamzahullah@yahoo.com>
2023-12-19 12:08:17 -0500

    fix: correct spelling of canceled

--- src/components/dashboard/main-content/course-enrollments/CourseAssignmentAlert.jsx
+++ src/components/dashboard/main-content/course-enrollments/CourseAssignmentAlert.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { AppContext } from '@edx/frontend-platform/react';
 import PropTypes from 'prop-types';
-import { Button, Alert, MailtoLink } from '@edx/paragon';
+import { Alert, Button, MailtoLink } from '@edx/paragon';
 import { Info } from '@edx/paragon/icons';
 import { getContactEmail } from '../../../../utils/common';
 
@@ -9,9 +9,9 @@ const CourseAssignmentAlert = ({
   onClose,
   variant,
 }) => {
-  const heading = variant === 'cancelled' ? 'Course assignment cancelled' : 'Deadline passed';
+  const heading = variant === 'cancelled' ? 'Course assignment canceled' : 'Deadline passed';
   const text = (variant === 'cancelled'
-    ? 'Your learning administrator cancelled one or more course assignments below.'
+    ? 'Your learning administrator canceled one or more course assignments below.'
     : 'Deadline to enroll into one or more courses below has passed.');
 
   const { enterpriseConfig } = useContext(AppContext);
