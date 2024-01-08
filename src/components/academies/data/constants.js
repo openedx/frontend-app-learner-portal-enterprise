dcdc7f366ac45f54199d6073935a3161797b6ead
muhammad-ammar <mammar@gmail.com>
2024-01-08 21:50:37 +0500

    fix: typo on acedemy detail page

--- src/components/academies/data/constants.js
+++ src/components/academies/data/constants.js
@@ -1,7 +1,7 @@
 export const ACADEMY_NOT_FOUND_TITLE = 'Academy not found';
 export const EXECUTIVE_EDUCATION_SECTION = {
   title: 'Executive Education',
-  subtitle: 'A selection of high-impact graduate-level courses that follow a strucured schedule and include active interaction with educators and peers.',
+  subtitle: 'A selection of high-impact graduate-level courses that follow a structured schedule and include active interaction with educators and peers.',
 };
 export const SELF_PACED_SECTION = {
   title: 'Self-paced courses',
