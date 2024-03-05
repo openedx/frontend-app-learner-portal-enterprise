c7cb496f20dd786399e1e42564aac62becd975d3
Adam Stankiewicz <agstanki@gmail.com>
2024-03-05 17:40:38 -0500

    chore: tests for useNotices and RouterFallback

--- src/components/app/data/hooks/useNotices.js
+++ src/components/app/data/hooks/useNotices.js
@@ -3,14 +3,19 @@ import { useQuery } from '@tanstack/react-query';
 
 import { queryNotices } from '../../routes/data';
 
+/**
+ * Responsible for returning the redirect URL for any notice(s) present
+ * for the authenticated user.
+ */
 function useNotices() {
   const queryResults = useQuery(queryNotices());
 
   useEffect(() => {
-    if (queryResults.data) {
-      window.location.assign(queryResults.data);
+    if (!queryResults.data) {
+      return;
     }
-  }, [queryResults]);
+    window.location.assign(queryResults.data);
+  }, [queryResults.data]);
 
   return queryResults;
 }
--- src/components/app/routes/index.js
+++ src/components/app/routes/index.js
@@ -1,1 +1,2 @@
 export { default as RouterFallback } from './RouterFallback';
+export * from './data';
