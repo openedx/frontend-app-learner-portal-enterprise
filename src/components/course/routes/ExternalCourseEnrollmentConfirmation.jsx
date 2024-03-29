65c00d239d17071008c15ee1db8a9d82e6b7d9ff
Adam Stankiewicz <agstanki@gmail.com>
2024-03-29 15:37:34 -0400

    fix: be consistent with the dashboard CTA and refactor

--- src/components/course/routes/ExternalCourseEnrollmentConfirmation.jsx
+++ src/components/course/routes/ExternalCourseEnrollmentConfirmation.jsx
@@ -1,32 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, generatePath, useParams } from 'react-router-dom';
 import { Button, Container } from '@openedx/paragon';
-import { getConfig } from '@edx/frontend-platform/config';
 
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
-import { useExternalEnrollmentFailureReason, useIsCourseAssigned, useMinimalCourseMetadata } from '../data/hooks';
+import { useExternalEnrollmentFailureReason } from '../data/hooks';
 import CourseSummaryCard from '../../executive-education-2u/components/CourseSummaryCard';
 import EnrollmentCompletedSummaryCard from '../../executive-education-2u/components/EnrollmentCompletedSummaryCard';
 import ErrorPageContent from '../../executive-education-2u/components/ErrorPageContent';
-import { useEnterpriseCustomer } from '../../app/data';
 
 const ExternalCourseEnrollmentConfirmation = () => {
-  const minimalCourseMetadata = useMinimalCourseMetadata();
+  const { enterpriseSlug } = useParams();
   const {
     failureReason,
     failureMessage,
   } = useExternalEnrollmentFailureReason();
 
-  const config = getConfig();
-  const { data: enterpriseCustomer } = useEnterpriseCustomer();
-  const isCourseAssigned = useIsCourseAssigned();
-  const externalDashboardQueryParams = new URLSearchParams({
-    org_id: enterpriseCustomer.authOrgId,
-  });
-  const externalDashboardQueryString = externalDashboardQueryParams ? `?${externalDashboardQueryParams.toString()}` : '';
-  const externalDashboardUrl = `${config.GETSMARTER_LEARNER_DASHBOARD_URL}${externalDashboardQueryString ?? ''}`;
-  const enterpriseSlug = `/${enterpriseCustomer.slug}`;
-  const dashboardUrl = `${config.BASE_URL}${enterpriseSlug}`;
-  const getStudentTCUrl = config.GETSMARTER_STUDENT_TC_URL;
   return (
     <div className="fill-vertical-space page-light-bg">
       {failureReason ? (
@@ -47,7 +34,7 @@ const ExternalCourseEnrollmentConfirmation = () => {
           <Button
             as={Link}
             className="mb-3 ml-auto"
-            destination={dashboardUrl}
+            to={generatePath('/:enterpriseSlug', { enterpriseSlug })}
           >
             <FormattedMessage
               id="executive.education.external.course.enrollment.completed.page.go.to.dashboard.button"
@@ -55,16 +42,8 @@ const ExternalCourseEnrollmentConfirmation = () => {
               description="Button that will navigate the learners to learner dashboard"
             />
           </Button>
-          <CourseSummaryCard
-            courseMetadata={minimalCourseMetadata}
-            enrollmentCompleted
-          />
-          <EnrollmentCompletedSummaryCard
-            isCourseAssigned={isCourseAssigned}
-            externalDashboardUrl={externalDashboardUrl}
-            dashboardUrl={dashboardUrl}
-            getStudentTCUrl={getStudentTCUrl}
-          />
+          <CourseSummaryCard enrollmentCompleted />
+          <EnrollmentCompletedSummaryCard />
         </Container>
       )}
     </div>
--- src/components/executive-education-2u/components/EnrollmentCompletedSummaryCard.jsx
+++ src/components/executive-education-2u/components/EnrollmentCompletedSummaryCard.jsx
@@ -1,132 +1,101 @@
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Link, generatePath, useParams } from 'react-router-dom';
+import { getConfig } from '@edx/frontend-platform';
 import {
   Card, Col, Hyperlink, Row,
 } from '@openedx/paragon';
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import GetSmarterLogo from '../../../assets/icons/get-smarter-logo-black.svg';
-import { features } from '../../../config';
 
-const EnrollmentCompletedSummaryCard = ({
-  isCourseAssigned,
-  externalDashboardUrl,
-  dashboardUrl,
-  getStudentTCUrl,
-}) => (
-  <Card className="bg-light-500">
-    <Row className="my-3">
-      <Col xs={12} md={3}>
-        <Card.Section>
-          <img
-            className="d-block"
-            src={GetSmarterLogo}
-            alt="partner-header-logo"
-            data-testid="partner-header-logo-image-id"
-          />
-        </Card.Section>
-      </Col>
-      <Col xs={12} md={9}>
-        <Card.Section>
-          <div className="h3 mb-4">
-            <FormattedMessage
-              id="executive.education.external.course.enrollment.completed.page.summary.card.title"
-              defaultMessage="What happens next?"
-              description="Title for the executive education course enrollment completed page summary card"
+const EnrollmentCompletedSummaryCard = () => {
+  const getStudentTCUrl = getConfig().GETSMARTER_STUDENT_TC_URL;
+  const { enterpriseSlug } = useParams();
+  return (
+    <Card className="bg-light-500">
+      <Row className="my-3">
+        <Col xs={12} md={3}>
+          <Card.Section>
+            <img
+              className="d-block"
+              src={GetSmarterLogo}
+              alt=""
+              data-testid="partner-header-logo-image-id"
             />
-          </div>
-          <div className="mb-3.5">
-            <div className="mb-1.5 text-black-color">
+          </Card.Section>
+        </Col>
+        <Col xs={12} md={9}>
+          <Card.Section>
+            <div className="h3 mb-4">
               <FormattedMessage
-                id="executive.education.external.course.enrollment.completed.page.summary.card.course.notified.about.email.section.heading"
-                defaultMessage="Notified by email"
-                description="Heading for the section about being notified by email to the learner"
+                id="executive.education.external.course.enrollment.completed.page.summary.card.title"
+                defaultMessage="What happens next?"
+                description="Title for the executive education course enrollment completed page summary card"
               />
             </div>
-            <div className="small mb-2 text-gray-500">
-              {
-                (features.FEATURE_ENABLE_TOP_DOWN_ASSIGNMENT && isCourseAssigned)
-                  ? (
-                    <FormattedMessage
-                      id="executive.education.external.course.enrollment.completed.page.summary.card.email.section.content.dashboard.link"
-                      defaultMessage="You will receive an email from edX when your course starts. Alternatively, you can visit your <a>edX dashboard</a> for course status updates."
-                      description="Content for the section about being notified by email to the learner with a edx dashboard link. And here edX is brand name"
-                      values={{
-                        // eslint-disable-next-line react/no-unstable-nested-components
-                        a: (chunks) => (
-                          <Link to={dashboardUrl}>
-                            {chunks}
-                          </Link>
-                        ),
-                      }}
-                    />
-                  ) : (
-                    <FormattedMessage
-                      id="executive.education.external.course.enrollment.completed.page.summary.card.email.section.content.with.getsmarter.link"
-                      defaultMessage="GetSmarter will email you when your course starts. Alternatively, you can visit your <a>GetSmarter learner dashboard</a> for course status updates."
-                      description="Content for the section about being notified by email to the learner with a getsmarter dashboard link. And here Getsmarter is brand name"
-                      values={{
-                        // eslint-disable-next-line react/no-unstable-nested-components
-                        a: (chunks) => (
-                          <Hyperlink
-                            destination={externalDashboardUrl}
-                            target="_blank"
-                          >
-                            {chunks}
-                          </Hyperlink>
-                        ),
-                      }}
-                    />
-                  )
-              }
+            <div className="mb-3.5">
+              <div className="mb-1.5 text-black-color">
+                <FormattedMessage
+                  id="executive.education.external.course.enrollment.completed.page.summary.card.course.notified.about.email.section.heading"
+                  defaultMessage="Notified by email"
+                  description="Heading for the section about being notified by email to the learner"
+                />
+              </div>
+              <div className="small mb-2 text-gray-500">
+                <FormattedMessage
+                  id="executive.education.external.course.enrollment.completed.page.summary.card.email.section.content.with.getsmarter.link"
+                  defaultMessage="You will receive an email when your course starts. Alternatively, you can visit your <a>dashboard</a> for course status updates."
+                  description="Content for the section about being notified by email to the learner with a dashboard link."
+                  values={{
+                    // eslint-disable-next-line react/no-unstable-nested-components
+                    a: (chunks) => (
+                      <Link to={generatePath('/:enterpriseSlug', { enterpriseSlug })}>
+                        {chunks}
+                      </Link>
+                    ),
+                  }}
+                />
+              </div>
             </div>
-          </div>
-          <div className="mb-3.5">
-            <div className="mb-1.5 text-black-color">
-              <FormattedMessage
-                id="executive.education.external.course.enrollment.completed.page.summary.card.course.refund.policy.section.heading"
-                defaultMessage="Read the refund policy"
-                description="Heading for the section about the refund policy for the executive education course"
-              />
+            <div className="mb-3.5">
+              <div className="mb-1.5 text-black-color">
+                <FormattedMessage
+                  id="executive.education.external.course.enrollment.completed.page.summary.card.course.refund.policy.section.heading"
+                  defaultMessage="Read the refund policy"
+                  description="Heading for the section about the refund policy for the executive education course"
+                />
+              </div>
+              <div className="small text-gray-500">
+                <FormattedMessage
+                  id="executive.education.external.course.enrollment.completed.page.summary.card.course.refund.policy.section.content"
+                  defaultMessage="As part of our commitment to your professional development, you may request to change
+                your course start date or request your money back if you're not fully satisfied with 14 calendar days
+                  of your course start date."
+                  description="Content for the section about the refund policy for the executive education course"
+                />
+              </div>
+              <div className="small mb-2 text-gray-500">
+                <FormattedMessage
+                  id="executive.education.external.course.enrollment.completed.page.summary.card.course.refund.policy.section.read.content"
+                  defaultMessage="Read GetSmarter's <a>Terms and Conditions</a> for the full course postponement and cancellation policy."
+                  description="Content to read more about getsmarter refund policy for the executive education course. And here GetSmarter is brand name"
+                  values={{
+                    // eslint-disable-next-line react/no-unstable-nested-components
+                    a: (chunks) => (
+                      <Hyperlink
+                        destination={getStudentTCUrl}
+                        target="_blank"
+                      >
+                        {chunks}
+                      </Hyperlink>
+                    ),
+                  }}
+                />
+              </div>
             </div>
-            <div className="small text-gray-500">
-              <FormattedMessage
-                id="executive.education.external.course.enrollment.completed.page.summary.card.course.refund.policy.section.content"
-                defaultMessage="As part of our commitment to your professional development, you may request to change
-               your course start date or request your money back if you're not fully satisfied with 14 calendar days
-                of your course start date."
-                description="Content for the section about the refund policy for the executive education course"
-              />
-            </div>
-            <div className="small mb-2 text-gray-500">
-              <FormattedMessage
-                id="executive.education.external.course.enrollment.completed.page.summary.card.course.refund.policy.section.read.content"
-                defaultMessage="Read GetSmarter's <a>Terms and Conditions</a> for the full course postponement and cancellation policy."
-                description="Content to read more about getsmarter refund policy for the executive education course. And here GetSmarter is brand name"
-                values={{
-                  // eslint-disable-next-line react/no-unstable-nested-components
-                  a: (chunks) => (
-                    <Hyperlink
-                      destination={getStudentTCUrl}
-                      target="_blank"
-                    >
-                      {chunks}
-                    </Hyperlink>
-                  ),
-                }}
-              />
-            </div>
-          </div>
-        </Card.Section>
-      </Col>
-    </Row>
-  </Card>
-);
-
-EnrollmentCompletedSummaryCard.propTypes = {
-  isCourseAssigned: PropTypes.bool.isRequired,
-  externalDashboardUrl: PropTypes.string.isRequired,
-  dashboardUrl: PropTypes.string.isRequired,
-  getStudentTCUrl: PropTypes.string.isRequired,
+          </Card.Section>
+        </Col>
+      </Row>
+    </Card>
+  );
 };
 
 export default EnrollmentCompletedSummaryCard;
