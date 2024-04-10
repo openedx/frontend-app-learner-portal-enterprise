64a3cd8de3fe92d7d846157ea3ae4ef5678839a9
Adam Stankiewicz <agstanki@gmail.com>
2024-04-10 15:46:59 -0400

    fix: extend default garbage collection time to 30 minutes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -30,20 +30,19 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: defaultQueryClientRetryHandler,
-      // Suspense mode on queries enables loading/error states to be caught and handled by a surrounding
-      // `Suspense` component from React, with a fallback UI component to display while the query is resolving.
-      // Generally, queries should be resolved within a route loader so it's "guaranteed" to exist within the UI
-      // components. However, `@tanstack/react-query` removes queries from the query cache when they are inactive
-      // (i.e., not rendered) for more than the default `cacheTime` (garbage collection time). To prevent this, by
-      // suspense, we can trigger a loading state with a `Suspense` fallback component while queries that were garbage
-      // collected are re-fetched again, instead of throwing a JS error.
-      suspense: true,
       // Specifying a longer `staleTime` of 20 seconds means queries will not refetch their data
       // as often; mitigates making duplicate queries when within the `staleTime` window, instead
       // relying on the cached data until the `staleTime` window has exceeded. This may be modified
       // per-query, as needed, if certain queries expect to be more up-to-date than others. Allows
       // `useQuery` to be used as a state manager.
-      staleTime: 1000 * 20,
+      staleTime: 1000 * 20, // 20 seconds
+      // By extending `cacheTime`from the default of 5 minutes, we can prevent inactive queries from being garbage
+      // collected for a longer duration of time. Inactive queries are those that have no rendered query observers
+      // (e.g., `useQuery` hooks). Since most UI component assumes data will be available and returned by queries
+      // without having to consider hard loading states, extending the `cacheTime` can help prevent JS errors around
+      // accessing properties on `undefined` data (due to it being in a hard loading state, `isLoading: true`) by
+      // delaying when `@tanstack/react-query` garbage collects inactive queries.
+      cacheTime: 1000 * 60 * 30, // 30 minutes
       // To prevent hard loading states if/when query keys change during automatic query background
       // re-fetches, we can set `keepPreviousData` to `true` to keep the previous data until the new
       // data is fetched. By enabling this option, UI components generally will not need to consider
@@ -52,6 +51,14 @@ const queryClient = new QueryClient({
       // https://tanstack.com/query/latest/docs/framework/vue/guides/migrating-to-v5#removed-keeppreviousdata-in-favor-of-placeholderdata-identity-function
       // for more details.
       keepPreviousData: true,
+      // Suspense mode on queries enables loading/error states to be caught and handled by a surrounding
+      // `Suspense` component from React, with a fallback UI component to display while the query is resolving.
+      // Generally, queries should be resolved within a route loader so it's "guaranteed" to exist within the UI
+      // components. However, in some cases (e.g., if a query is reset), we attempt to access object properties
+      // on `undefined` data (i.e., `isLoading: true`) resulting in JS errors. To prevent this error from throwing,
+      // by enabling suspenseful queries, we can trigger a loading state via a `Suspense` fallback component while
+      // queries that were removed/reset/garbage collected are re-fetched.
+      suspense: true,
     },
   },
 });
