695e2f74e622271311d55e58c7c764b4849197a2
Adam Stankiewicz <agstanki@gmail.com>
2024-04-12 09:09:11 -0400

    chore: more test updates

--- src/components/app/data/hooks/usePassLearnerCsodParams.js
+++ src/components/app/data/hooks/usePassLearnerCsodParams.js
@@ -1,33 +1,40 @@
 import { useEffect } from 'react';
+import { logError } from '@edx/frontend-platform/logging';
 import { useLocation, useParams } from 'react-router-dom';
 import { updateUserCsodParams } from '../services';
+import useEnterpriseCustomer from './useEnterpriseCustomer';
 
-const usePassLearnerCsodParams = (enterpriseUUID) => {
+const usePassLearnerCsodParams = () => {
   const location = useLocation();
   const { courseKey } = useParams();
+  const { data: enterpriseCustomer } = useEnterpriseCustomer();
 
-  useEffect(async () => {
+  useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
     const userGuid = queryParams.get('userGuid');
     const sessionToken = queryParams.get('sessionToken');
-
-    if (userGuid && sessionToken && courseKey && enterpriseUUID) {
-      const data = {
-        userGuid,
-        sessionToken,
-        callbackUrl: queryParams.get('callbackUrl'),
-        subdomain: queryParams.get('subdomain'),
-        courseKey,
-        enterpriseUUID,
-      };
-      try {
-        await updateUserCsodParams({data}); // Call updateUserCsodParams function
-        // Handle successful response if needed
-      } catch (error) {
-        console.error('An error occurred:', error);
-      }
+    if (!userGuid || !sessionToken || !courseKey) {
+      return;
     }
-  }, [location.search, courseKey, enterpriseUUID]);
+    const callbackUrl = queryParams.get('callbackUrl');
+    const subdomain = queryParams.get('subdomain');
+    const data = {
+      userGuid,
+      sessionToken,
+      callbackUrl,
+      subdomain,
+      courseKey,
+      enterpriseUUID: enterpriseCustomer.uuid,
+    };
+    const performCsodParamsUpdate = async () => {
+      try {
+        await updateUserCsodParams({ data });
+      } catch (error) {
+        logError(error);
+      }
+    };
+    performCsodParamsUpdate();
+  }, [location.search, courseKey, enterpriseCustomer.uuid]);
 };
 
 export default usePassLearnerCsodParams;
--- src/components/course/enrollment/tests/ToEcomBasketPage.test.jsx
+++ src/components/course/enrollment/tests/ToEcomBasketPage.test.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import * as hooks from '../hooks';
 import ToEcomBasketPage from '../components/ToEcomBasketPage';
-import { CourseContext } from '../../CourseContextProvider';
-import { useTrackSearchConversionClickHandler } from '../../data';
+import { useTrackSearchConversionClickHandler, useUserSubsidyApplicableToCourse } from '../../data';
+import { useCouponCodes, useCourseMetadata, useEnterpriseCourseEnrollments } from '../../../app/data';
 
 jest.mock('../common', () => ({
   __esModule: true,
@@ -17,49 +16,39 @@ jest.mock('../../EnrollModal', () => ({
   default: () => <span>EnrollModal</span>,
 }));
 
-jest.mock('../hooks');
+jest.mock('../../../app/data', () => ({
+  ...jest.requireActual('../../../app/data'),
+  useCouponCodes: jest.fn(),
+  useCourseMetadata: jest.fn(),
+  useEnterpriseCourseEnrollments: jest.fn(),
+}));
+
 jest.mock('../../data', () => ({
   ...jest.requireActual('../../data'),
   useOptimizelyEnrollmentClickHandler: jest.fn(),
   useTrackSearchConversionClickHandler: jest.fn(),
+  useUserSubsidyApplicableToCourse: jest.fn(),
 }));
 const mockTrackSearchConversionClickHandler = jest.fn();
-
-const ToEcomBasketPageWrapper = ({
-  courseContextValue = {
-    state: {
-      activeCourseRun: {
-        key: 'course-key',
-      },
-      userEnrollments: [],
-    },
-  },
-  ...rest
-}) => (
-  <CourseContext.Provider value={courseContextValue}>
-    <ToEcomBasketPage {...rest} />,
-  </CourseContext.Provider>
-);
 
 describe('<ToEcomBasketPage />', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     useTrackSearchConversionClickHandler.mockReturnValue(mockTrackSearchConversionClickHandler);
+    useUserSubsidyApplicableToCourse.mockReturnValue({ userSubsidyApplicableToCourse: undefined });
+    useCouponCodes.mockReturnValue({ data: { couponCodeAssignments: [] } });
+    useCourseMetadata.mockReturnValue({ data: { activeCourseRun: { key: 'course-run-key' } } });
+    useEnterpriseCourseEnrollments.mockReturnValue({ data: { enterpriseCourseEnrollments: [] } });
   });
 
   it('should render <EnrollButtonCta /> and <EnrollModal />', () => {
-    hooks.useSubsidyDataForCourse.mockReturnValue(
-      { userSubsidyApplicableToCourse: undefined, couponCodesCount: 0 },
-    );
-
     render(
-      <ToEcomBasketPageWrapper
+      <ToEcomBasketPage
         enrollLabel="enroll"
         enrollmentUrl="enroll_url"
         courseRunPrice={100}
       />,
     );
-
     expect(screen.getByText('EnrollButtonCta')).toBeInTheDocument();
     expect(screen.getByText('EnrollModal')).toBeInTheDocument();
   });
