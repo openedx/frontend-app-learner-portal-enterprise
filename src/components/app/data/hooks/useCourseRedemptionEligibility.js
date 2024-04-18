3ab797a8cc21bf3c59b83157ebe27731b5f7338d
Hamzah Ullah <hamzahullah@yahoo.com>
2024-04-18 09:59:23 -0400

    feat: include useCourseRedemptionEligibility tests

--- src/components/app/data/hooks/useCourseRedemptionEligibility.js
+++ src/components/app/data/hooks/useCourseRedemptionEligibility.js
@@ -6,6 +6,36 @@ import { queryCanRedeem } from '../queries';
 import useEnterpriseCustomer from './useEnterpriseCustomer';
 import useLateRedemptionBufferDays from './useLateRedemptionBufferDays';
 
+export function transformCourseRedemptionEligibility({
+  courseMetadata,
+  canRedeemData,
+  courseRunKey,
+}) {
+  const redeemabilityForActiveCourseRun = canRedeemData.find(r => r.contentKey === courseMetadata.activeCourseRun?.key);
+  const missingSubsidyAccessPolicyReason = redeemabilityForActiveCourseRun?.reasons[0];
+  const preferredSubsidyAccessPolicy = redeemabilityForActiveCourseRun?.redeemableSubsidyAccessPolicy;
+  const otherSubsidyAccessPolicy = canRedeemData.find(
+    r => r.redeemableSubsidyAccessPolicy,
+  )?.redeemableSubsidyAccessPolicy;
+  const listPrice = redeemabilityForActiveCourseRun?.listPrice?.usd;
+  const hasSuccessfulRedemption = courseRunKey
+    ? !!canRedeemData.find(r => r.contentKey === courseRunKey)?.hasSuccessfulRedemption
+    : canRedeemData.some(r => r.hasSuccessfulRedemption);
+
+  // If there is a redeemable subsidy access policy for the active course run, use that. Otherwise, use any other
+  // redeemable subsidy access policy for any of the content keys.
+  const redeemableSubsidyAccessPolicy = preferredSubsidyAccessPolicy || otherSubsidyAccessPolicy;
+  const isPolicyRedemptionEnabled = hasSuccessfulRedemption || !!redeemableSubsidyAccessPolicy;
+  return {
+    isPolicyRedemptionEnabled,
+    redeemabilityPerContentKey: canRedeemData,
+    redeemableSubsidyAccessPolicy,
+    missingSubsidyAccessPolicyReason,
+    hasSuccessfulRedemption,
+    listPrice,
+  };
+}
+
 /**
  * Retrieves the course redemption eligibility for the given enterprise customer and course key.
  * @returns {Types.UseQueryResult}} The query results for the course redemption eligibility.
@@ -21,31 +51,11 @@ export default function useCourseRedemptionEligibility(queryOptions = {}) {
     ...queryCanRedeem(enterpriseCustomer.uuid, courseMetadata, isEnrollableBufferDays),
     enabled: !!courseMetadata,
     select: (data) => {
-      const redeemabilityForActiveCourseRun = data.find(r => r.contentKey === courseMetadata.activeCourseRun?.key);
-      const missingSubsidyAccessPolicyReason = redeemabilityForActiveCourseRun?.reasons[0];
-      const preferredSubsidyAccessPolicy = redeemabilityForActiveCourseRun?.redeemableSubsidyAccessPolicy;
-      const otherSubsidyAccessPolicy = data.find(
-        r => r.redeemableSubsidyAccessPolicy,
-      )?.redeemableSubsidyAccessPolicy;
-      const listPrice = redeemabilityForActiveCourseRun?.listPrice?.usd;
-
-      const hasSuccessfulRedemption = courseRunKey
-        ? !!data.find(r => r.contentKey === courseRunKey)?.hasSuccessfulRedemption
-        : data.some(r => r.hasSuccessfulRedemption);
-
-      // If there is a redeemable subsidy access policy for the active course run, use that. Otherwise, use any other
-      // redeemable subsidy access policy for any of the content keys.
-      const redeemableSubsidyAccessPolicy = preferredSubsidyAccessPolicy || otherSubsidyAccessPolicy;
-      const isPolicyRedemptionEnabled = hasSuccessfulRedemption || !!redeemableSubsidyAccessPolicy;
-
-      const transformedData = {
-        isPolicyRedemptionEnabled,
-        redeemabilityPerContentKey: data,
-        redeemableSubsidyAccessPolicy,
-        missingSubsidyAccessPolicyReason,
-        hasSuccessfulRedemption,
-        listPrice,
-      };
+      const transformedData = transformCourseRedemptionEligibility({
+        courseMetadata,
+        canRedeemData: data,
+        courseRunKey,
+      });
       if (select) {
         return select({
           original: data,
