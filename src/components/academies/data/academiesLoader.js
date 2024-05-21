368e891d43d40244225483d9ca760947523eb39e
irfanuddinahmad <iahmad@2u.com>
2024-05-21 20:07:01 +0500

    feat: Added enterprise customer parameter for academies detail api

--- src/components/academies/data/academiesLoader.js
+++ src/components/academies/data/academiesLoader.js
@@ -1,19 +1,23 @@
 import { ensureAuthenticatedUser } from '../../app/routes/data';
-import { queryAcademiesDetail } from '../../app/data';
+import { extractEnterpriseCustomer, queryAcademiesDetail } from '../../app/data';
 
 export default function makeAcademiesLoader(queryClient) {
   return async function academiesLoader({ params = {}, request }) {
     const requestUrl = new URL(request.url);
-
     const authenticatedUser = await ensureAuthenticatedUser(requestUrl, params);
     // User is not authenticated, so we can't do anything in this loader.
     if (!authenticatedUser) {
       return null;
     }
 
-    const { academyUUID } = params;
+    const { academyUUID, enterpriseSlug } = params;
+    const enterpriseCustomer = await extractEnterpriseCustomer({
+      queryClient,
+      authenticatedUser,
+      enterpriseSlug,
+    });
 
-    await queryClient.ensureQueryData(queryAcademiesDetail(academyUUID));
+    await queryClient.ensureQueryData(queryAcademiesDetail(academyUUID, enterpriseCustomer.uuid));
 
     return null;
   };
