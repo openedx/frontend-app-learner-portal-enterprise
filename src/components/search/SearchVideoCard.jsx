261da170504b31d3dcab0806301a92a898d14d41
Ejaz Ahmad <86868918+jajjibhai008@users.noreply.github.com>
2024-07-15 15:28:12 +0500

    fix: duration format on video cards (#1116)

--- src/components/search/SearchVideoCard.jsx
+++ src/components/search/SearchVideoCard.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import cardFallbackImg from '@edx/brand/paragon/images/card-imagecap-fallback.png';
 import { Link } from 'react-router-dom';
 import { camelCaseObject } from '@edx/frontend-platform/utils';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
 import {
   Card, Chip, Icon, Truncate,
 } from '@openedx/paragon';
@@ -10,6 +12,8 @@ import {
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import { PlayCircleOutline } from '@openedx/paragon/icons';
 import { useEnterpriseCustomer } from '../app/data';
+
+dayjs.extend(duration);
 
 const SearchVideoCard = ({
   key, hit, isLoading, parentRoute, ...rest
@@ -34,7 +38,12 @@ const SearchVideoCard = ({
     },
     [video, enterpriseCustomer.slug],
   );
-
+  const getFormattedDuration = (durationInSeconds) => {
+    const time = dayjs.duration({ seconds: durationInSeconds });
+    const minutes = Math.floor(time.asMinutes());
+    const seconds = (durationInSeconds % 60).toFixed(0).padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  };
   return (
     <Card
       data-testid="search-video-card"
@@ -64,7 +73,7 @@ const SearchVideoCard = ({
       <Card.Section />
       <Card.Footer className="justify-content-between">
         <div className="d-flex flex-column">
-          {video?.duration && <Chip size="lg" className="mb-2 px-0.5">{video.duration}</Chip>}
+          {video?.duration && <Chip size="lg" className="mb-2 px-0.5">{getFormattedDuration(video.duration)}</Chip>}
           <span className="text-muted x-small mx-1">
             <FormattedMessage
               id="enterprise.search.videoCard.generalLength.video"
