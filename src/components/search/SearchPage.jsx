42c06b0e09490c195e870d2c71cb7136bc976f9a
Maham Akif <113524403+mahamakifdar19@users.noreply.github.com>
2024-08-08 14:19:48 +0500

    fix: hide video section on search page for learners with inactive susbcription (#1144)
    Co-authored-by: Maham Akif <maham.akif@A006-00821.local>

--- src/components/search/SearchPage.jsx
+++ src/components/search/SearchPage.jsx
@@ -3,26 +3,25 @@ import { SearchData } from '@edx/frontend-enterprise-catalog-search';
 import { useIntl } from '@edx/frontend-platform/i18n';
 
 import { useSubscriptions } from '../app/data';
-import { LICENSE_STATUS } from '../enterprise-user-subsidy/data/constants';
 import Search from './Search';
 import { SEARCH_TRACKING_NAME } from './constants';
-import { getSearchFacetFilters } from './utils';
+import { getSearchFacetFilters, hasActivatedAndCurrentSubscription } from './utils';
 import { features } from '../../config';
 
 const SearchPage = () => {
   const intl = useIntl();
 
   const { data: { subscriptionLicense } } = useSubscriptions();
-  const hasActivatedAndCurrentSubscription = (
-    subscriptionLicense?.status === LICENSE_STATUS.ACTIVATED
-    && subscriptionLicense?.subscriptionPlan?.isCurrent
+  const enableVideos = (
+    features.FEATURE_ENABLE_VIDEO_CATALOG
+    && hasActivatedAndCurrentSubscription(subscriptionLicense)
   );
 
   return (
     <SearchData
       searchFacetFilters={getSearchFacetFilters(intl)}
       trackingName={SEARCH_TRACKING_NAME}
-      enableVideos={features.FEATURE_ENABLE_VIDEO_CATALOG && hasActivatedAndCurrentSubscription}
+      enableVideos={enableVideos}
     >
       <Search />
     </SearchData>
