d5ea4f5e7ae073f84cb551facf6d2d6615dadf40
Adam Stankiewicz <agstanki@gmail.com>
2024-10-10 10:40:24 -0400

    feat: incrementally adopt Dashboard BFF API

---
+++ src/components/app/data/services/bffs.js
@@ -0,0 +1,21 @@
+import { getConfig } from '@edx/frontend-platform/config';
+import { logError } from '@edx/frontend-platform/logging';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+import { camelCaseObject } from '@edx/frontend-platform/utils';
+
+export async function fetchEnterpriseLearnerDashboard(enterpriseId, lmsUserId) {
+  const { ENTERPRISE_ACCESS_BASE_URL } = getConfig();
+  const params = {
+    enterprise_customer_uuid: enterpriseId,
+    lms_user_id: lmsUserId,
+  };
+  const url = `${ENTERPRISE_ACCESS_BASE_URL}/api/v1/bffs/learner/dashboard/`;
+  try {
+    const result = await getAuthenticatedHttpClient().post(url, params);
+    return camelCaseObject(result.data);
+  } catch (error) {
+    logError(error);
+    // TODO: consider returning a sane default API response structure here to mitigate complete failure.
+    return {};
+  }
+}
