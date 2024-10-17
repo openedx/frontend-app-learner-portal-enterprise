47deed39c8fc0fe3941a49ec08dbf97e88d1d302
jajjibhai008 <ejazofficial122@gmail.com>
2024-10-17 17:05:48 +0500

    feat: updated the custom expiration model design and truly blocking

--- src/components/expired-subscription-modal/tests/ExpiredSubscriptionModal.test.jsx
+++ src/components/expired-subscription-modal/tests/ExpiredSubscriptionModal.test.jsx
@@ -15,9 +15,10 @@ describe('<ExpiredSubscriptionModal />', () => {
       data: {
         customerAgreement: {
           hasCustomLicenseExpirationMessaging: false,
+          modalHeaderText: null,
+          buttonLabelInModal: null,
           expiredSubscriptionModalMessaging: null,
-          urlForExpiredModal: null,
-          hyperLinkTextForExpiredModal: null,
+          urlForButtonInModal: null,
         },
       },
     });
@@ -33,18 +34,18 @@ describe('<ExpiredSubscriptionModal />', () => {
       data: {
         customerAgreement: {
           hasCustomLicenseExpirationMessaging: true,
-          expiredSubscriptionModalMessaging: 'Your subscription has expired.',
-          urlForExpiredModal: '/renew',
-          hyperLinkTextForExpiredModal: 'Click here to renew',
+          modalHeaderText: 'Expired Subscription',
+          buttonLabelInModal: 'Continue Learning',
+          expiredSubscriptionModalMessaging: '<p>Your subscription has expired.</p>',
+          urlForButtonInModal: '/renew',
         },
       },
     });
 
     renderWithRouter(<ExpiredSubscriptionModal />);
 
-    expect(screen.getByText('Your subscription has expired.')).toBeInTheDocument();
-    expect(screen.getByText('Click here to renew')).toBeInTheDocument();
-    expect(screen.getByRole('link', { name: 'Click here to renew' })).toHaveAttribute('href', '/renew');
+    expect(screen.getByText('Expired Subscription')).toBeInTheDocument();
+    expect(screen.getByText('Continue Learning')).toBeInTheDocument();
   });
 
   test('does not renderwithrouter modal if no customer agreement data is present', () => {
@@ -53,19 +54,20 @@ describe('<ExpiredSubscriptionModal />', () => {
     expect(container).toBeEmptyDOMElement();
   });
 
-  test('renderwithrouters close button in modal', () => {
+  test('Close button (cross) should not be available, making the modal truly blocking', () => {
     useSubscriptions.mockReturnValue({
       data: {
         customerAgreement: {
           hasCustomLicenseExpirationMessaging: true,
-          expiredSubscriptionModalMessaging: 'Subscription expired',
-          urlForExpiredModal: '/renew',
-          hyperLinkTextForExpiredModal: 'Renew',
+          modalHeaderText: 'Expired Subscription',
+          buttonLabelInModal: 'Continue Learning',
+          expiredSubscriptionModalMessaging: '<p>Your subscription has expired.</p>',
+          urlForButtonInModal: '/renew',
         },
       },
     });
 
     renderWithRouter(<ExpiredSubscriptionModal />);
-    expect(screen.getByRole('button', { name: /close/i })).toBeInTheDocument();
+    expect(screen.queryByLabelText(/close/i)).not.toBeInTheDocument();
   });
 });
