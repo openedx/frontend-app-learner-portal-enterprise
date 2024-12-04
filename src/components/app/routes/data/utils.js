b7eaa10f314c6f7bdc631328c2db0b499030451b
Hamzah Ullah <hamzahullah@yahoo.com>
2024-12-04 17:16:29 -0500

    fix: update conditionally blocked BFF queries (#1232)

--- src/components/app/routes/data/utils.js
+++ src/components/app/routes/data/utils.js
@@ -101,33 +101,35 @@ export async function ensureEnterpriseAppData({
 
         return subscriptionsData;
       }),
-      queryClient.ensureQueryData(
-        queryRedeemablePolicies({
-          enterpriseUuid: enterpriseCustomer.uuid,
-          lmsUserId: userId,
-        }),
-      ),
-      queryClient.ensureQueryData(
-        queryCouponCodes(enterpriseCustomer.uuid),
-      ),
-      queryClient.ensureQueryData(
-        queryEnterpriseLearnerOffers(enterpriseCustomer.uuid),
-      ),
-      queryClient.ensureQueryData(
-        queryBrowseAndRequestConfiguration(enterpriseCustomer.uuid),
-      ),
-      queryClient.ensureQueryData(
-        queryLicenseRequests(enterpriseCustomer.uuid, userEmail),
-      ),
-      queryClient.ensureQueryData(
-        queryCouponCodeRequests(enterpriseCustomer.uuid, userEmail),
-      ),
-      // Content Highlights
-      queryClient.ensureQueryData(
-        queryContentHighlightsConfiguration(enterpriseCustomer.uuid),
-      ),
     );
   }
+  enterpriseAppDataQueries.push(...[
+    queryClient.ensureQueryData(
+      queryRedeemablePolicies({
+        enterpriseUuid: enterpriseCustomer.uuid,
+        lmsUserId: userId,
+      }),
+    ),
+    queryClient.ensureQueryData(
+      queryCouponCodes(enterpriseCustomer.uuid),
+    ),
+    queryClient.ensureQueryData(
+      queryEnterpriseLearnerOffers(enterpriseCustomer.uuid),
+    ),
+    queryClient.ensureQueryData(
+      queryBrowseAndRequestConfiguration(enterpriseCustomer.uuid),
+    ),
+    queryClient.ensureQueryData(
+      queryLicenseRequests(enterpriseCustomer.uuid, userEmail),
+    ),
+    queryClient.ensureQueryData(
+      queryCouponCodeRequests(enterpriseCustomer.uuid, userEmail),
+    ),
+    // Content Highlights
+    queryClient.ensureQueryData(
+      queryContentHighlightsConfiguration(enterpriseCustomer.uuid),
+    ),
+  ]);
 
   if (getConfig().ENABLE_NOTICES) {
     enterpriseAppDataQueries.push(
