ef6215fe3b50f408798e5ead921dc027f0da9992
Adam Stankiewicz <agstanki@gmail.com>
2025-01-24 10:41:24 -0500

    fix: ensure relevant queries are pre-seeded for video and program progress routes

--- src/components/microlearning/data/videosLoader.test.jsx
+++ src/components/microlearning/data/videosLoader.test.jsx
@@ -1,11 +1,16 @@
-/* eslint-disable react/jsx-filename-extension */
+import { when } from 'jest-when';
 import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import { renderWithRouterProvider } from '../../../utils/tests';
 import makeVideosLoader from './videosLoader';
 import { ensureAuthenticatedUser } from '../../app/routes/data';
-import { extractEnterpriseCustomer, queryVideoDetail } from '../../app/data';
+import {
+  extractEnterpriseCustomer,
+  queryCourseMetadata,
+  queryCourseReviews,
+  queryVideoDetail,
+} from '../../app/data';
 import { authenticatedUserFactory, enterpriseCustomerFactory } from '../../app/data/services/data/__factories__';
 
 jest.mock('../../app/routes/data', () => ({
@@ -26,7 +31,7 @@ const mockVideoUUID = 'test-video-uuid';
 const mockVideosURL = `/${mockEnterpriseSlug}/videos/${mockVideoUUID}/`;
 
 const mockQueryClient = {
-  ensureQueryData: jest.fn().mockResolvedValue({}),
+  ensureQueryData: jest.fn().mockResolvedValue(),
 };
 
 describe('videosLoader', () => {
@@ -40,7 +45,7 @@ describe('videosLoader', () => {
     ensureAuthenticatedUser.mockResolvedValue(null);
     renderWithRouterProvider(
       {
-        path: '/:enterpriseSlug/videos/:videoUUID/',
+        path: '/:enterpriseSlug/videos/:videoUUID',
         element: <div>hello world</div>,
         loader: makeVideosLoader(mockQueryClient),
       },
@@ -54,10 +59,34 @@ describe('videosLoader', () => {
     expect(mockQueryClient.ensureQueryData).not.toHaveBeenCalled();
   });
 
-  it('ensures the requisite video data is resolved', async () => {
+  it.each([
+    { hasVideoDetailData: true },
+    { hasVideoDetailData: false },
+  ])('ensures the requisite video data is resolved', async ({ hasVideoDetailData }) => {
+    const mockCourseKey = 'test-course-key';
+    const videoDetailQuery = queryVideoDetail(mockVideoUUID, mockEnterpriseId);
+    const courseMetadataQuery = queryCourseMetadata(mockCourseKey);
+    const courseReviewsQuery = queryCourseReviews(mockCourseKey);
+
+    // Mock the resolved data for the queries
+    when(mockQueryClient.ensureQueryData).calledWith(
+      expect.objectContaining({
+        queryKey: videoDetailQuery.queryKey,
+      }),
+    ).mockResolvedValue(hasVideoDetailData ? { courseKey: mockCourseKey } : null);
+    when(mockQueryClient.ensureQueryData).calledWith(
+      expect.objectContaining(courseMetadataQuery),
+    ).mockResolvedValue({
+      key: mockCourseKey,
+    });
+    when(mockQueryClient.ensureQueryData).calledWith(
+      expect.objectContaining(courseReviewsQuery),
+    ).mockResolvedValue([]);
+
+    // Render the route with videosLoader
     renderWithRouterProvider(
       {
-        path: '/:enterpriseSlug/videos/:videoUUID/',
+        path: '/:enterpriseSlug/videos/:videoUUID',
         element: <div>hello world</div>,
         loader: makeVideosLoader(mockQueryClient),
       },
@@ -68,7 +97,8 @@ describe('videosLoader', () => {
 
     expect(await screen.findByText('hello world')).toBeInTheDocument();
 
-    expect(mockQueryClient.ensureQueryData).toHaveBeenCalledTimes(1);
+    const expectedQueryCount = hasVideoDetailData ? 3 : 1;
+    expect(mockQueryClient.ensureQueryData).toHaveBeenCalledTimes(expectedQueryCount);
     expect(mockQueryClient.ensureQueryData).toHaveBeenCalledWith(
       expect.objectContaining({
         queryKey: queryVideoDetail(mockVideoUUID, mockEnterpriseId).queryKey,
