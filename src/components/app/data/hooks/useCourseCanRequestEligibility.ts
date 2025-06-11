0070aee953080499ab857f1a657d0d78cf5e5f4d
Ahmad Hassan <ahmad.hassan02@A006-01206.local>
2025-06-11 11:49:53 +0500

    feat: add can_request eligibility hook

---
+++ src/components/app/data/hooks/useCourseCanRequestEligibility.ts
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import useEnterpriseCustomer from './useEnterpriseCustomer';
+import { queryCanRequest } from '../queries';
+
+/**
+ * Retrieves the can_request eligibility for the given enterprise customer and course key.
+ * @returns The query results for the can_request eligibility.
+ */
+export default function useCourseCanRequestEligibility() {
+  const { data: enterpriseCustomer } = useEnterpriseCustomer<EnterpriseCustomer>();
+  const { courseKey } = useParams();
+
+  return useQuery(queryCanRequest(enterpriseCustomer.uuid, courseKey!));
+}
